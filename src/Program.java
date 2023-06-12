3dbd30a0d44789d17ebc44b40389401cc74a4114
dannnik4 <daniilperstnev2001@gmail.com>
2023-06-12 13:41:04 +0300

    Updated Program.java

--- src/Program.java
+++ src/Program.java
@@ -1,7 +1,9 @@
 public class Program {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello Java!");
+        {
+            System.out.println("Hello!");
+            System.out.println("Welcome to Java!");
+        }
     }
 }
