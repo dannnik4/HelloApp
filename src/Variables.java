51117c3f296e00efd073f82164423dc5c6157c2f
dannnik4 <daniilperstnev2001@gmail.com>
2023-06-13 20:58:49 +0300

    Updated Variables.java

--- src/Variables.java
+++ src/Variables.java
@@ -1,7 +1,10 @@
 public class Variables {
     public static void main(String args[]) {
-        int x;      // объявление переменной
-        x = 10;     // присвоение значения
+//        int x;      // объявление переменной
+//        x = 10;     // присвоение значения
+//        System.out.println(x);  // 10
+
+        int x = 10;     // объявление и инициализация переменной
         System.out.println(x);  // 10
     }
 }
