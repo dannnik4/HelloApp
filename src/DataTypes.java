2d6a8695f69b640a23e19b2cdfb81737f00d491e
dannnik4 <daniilperstnev2001@gmail.com>
2023-06-14 14:43:10 +0300

    Updated DataTypes.java

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -36,5 +36,28 @@ public class DataTypes {
 
         float fl = 30.6f;
         double db = 30.6;
+
+        char ch=102; // символ 'f'
+        System.out.println(ch);
+
+        String hello = "Hello...";
+        System.out.println(hello);
+
+        String text = "Hello \nworld";
+        System.out.println(text);
+
+        String text1 = "Вот мысль, которой весь я предан,\n"+
+                "Итог всего, что ум скопил.\n"+
+                "Лишь тот, кем бой за жизнь изведан,\n"+
+                "Жизнь и свободу заслужил.";
+        System.out.println(text1);
+
+        String text2 = """
+                Вот мысль, которой весь я предан,
+                Итог всего, что ум скопил.
+                Лишь тот, кем бой за жизнь изведан,
+                Жизнь и свободу заслужил.
+                """;
+        System.out.println(text2);
     }
 }
