23f1b5d471e7f0567fde73796ecd42956422a9f8
dannnik4 <daniilperstnev2001@gmail.com>
2023-06-15 09:49:32 +0300

    Updated ConsoleInOut.java

--- src/ConsoleInOut.java
+++ src/ConsoleInOut.java
@@ -13,5 +13,11 @@ public class ConsoleInOut {
         System.out.println("x=" + x + "; y=" + y);
 
         System.out.printf("x=%d; y=%d \n", x, y);
+
+        String name = "Tom";
+        int age = 30;
+        float height = 1.7f;
+
+        System.out.printf("Name: %s  Age: %d  Height: %.2f \n", name, age, height);
     }
 }
