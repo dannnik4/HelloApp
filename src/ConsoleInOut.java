3aeee1e8595c98dbda6dc5567ba17a25214331df
dannnik4 <daniilperstnev2001@gmail.com>
2023-06-15 09:54:48 +0300

    Updated ConsoleInOut.java

--- src/ConsoleInOut.java
+++ src/ConsoleInOut.java
@@ -27,6 +27,14 @@ public class ConsoleInOut {
         int num = in.nextInt();
 
         System.out.printf("Your number: %d \n", num);
+
+        System.out.print("Input name: ");
+        String name1 = in.nextLine();
+        System.out.print("Input age: ");
+        int age1 = in.nextInt();
+        System.out.print("Input height: ");
+        float height1 = in.nextFloat();
+        System.out.printf("Name: %s  Age: %d  Height: %.2f \n", name1, age1, height1);
         in.close();
     }
 }
