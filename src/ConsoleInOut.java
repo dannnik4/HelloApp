d983ed9c15194724057a566f4b494a95fcf5d942
dannnik4 <daniilperstnev2001@gmail.com>
2023-06-15 09:46:46 +0300

    Updated ConsoleInOut.java

--- src/ConsoleInOut.java
+++ src/ConsoleInOut.java
@@ -2,5 +2,8 @@ public class ConsoleInOut {
     public static void main(String args[]) {
         System.out.println("Hello world!");
         System.out.println("Bye world...");
+
+        System.out.print("Hello world!");
+        System.out.print("Bye world...");
     }
 }
