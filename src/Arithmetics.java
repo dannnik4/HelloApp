ea24508a722ae18cd29d551fe5929f46d74256d2
dannnik4 <daniilperstnev2001@gmail.com>
2023-06-16 10:16:11 +0300

    Updated Arithmetics.java

--- src/Arithmetics.java
+++ src/Arithmetics.java
@@ -2,6 +2,7 @@ public class Arithmetics {
     public static void main(String args[]) {
         int a = 10;
         int b = 7;
+
         int c = a + b;  // 17
         int d = 4 + b;  // 11
 
@@ -10,5 +11,10 @@ public class Arithmetics {
 
         int c2 = a * b;  // 70
         int d2 = b * 5;  // 35
+
+        a = 20;
+        b = 5;
+        int c3 = a / b;  // 4
+        double d3 = 22.5 / 4.5;  // 5.0
     }
 }
