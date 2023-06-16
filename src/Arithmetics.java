5be2097658fa92dec84a78f816141fa8b82e32f8
dannnik4 <daniilperstnev2001@gmail.com>
2023-06-16 10:15:04 +0300

    Added Arithmetics.java

--- src/Arithmetics.java
+++ src/Arithmetics.java
@@ -4,5 +4,10 @@ public class Arithmetics {
         int b = 7;
         int c = a + b;  // 17
         int d = 4 + b;  // 11
+
+        int a1 = 10;
+        int b2 = 7;
+        int c3 = a - b;  // 3
+        int d4 = 4 - a;  // -6
     }
 }
