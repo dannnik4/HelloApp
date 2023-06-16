b4ec205ba34029a80b3a0dbbe5cb59879daf56b2
dannnik4 <daniilperstnev2001@gmail.com>
2023-06-16 10:14:39 +0300

    Added Arithmetics.java

---
+++ src/Arithmetics.java
@@ -0,0 +1,8 @@
+public class Arithmetics {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 7;
+        int c = a + b;  // 17
+        int d = 4 + b;  // 11
+    }
+}
