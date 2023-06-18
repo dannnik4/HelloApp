d84c8e76dac997143cfe188b41295e19a816d981
dannnik4 <daniilperstnev2001@gmail.com>
2023-06-18 11:47:17 +0300

    Updated BitwiseOperations.java

--- src/BitwiseOperations.java
+++ src/BitwiseOperations.java
@@ -2,5 +2,13 @@ public class BitwiseOperations {
     public static void main(String args[]) {
         byte b = 7;     // 0000 0111
         short s = 7;    // 0000 0000 0000 0111
+
+        int a1 = 2; //010
+        int b1 = 5;//101
+        System.out.println(a1&b1); // результат 0
+
+        int a2 = 4; //100
+        int b2 = 5; //101
+        System.out.println(a2 & b2); // результат 4
     }
 }
