c7397d9913e449cf1c85df739ed20c825d7b6eff
dannnik4 <daniilperstnev2001@gmail.com>
2023-06-19 21:48:29 +0300

    Updated ConditionalExpressions.java

--- src/ConditionalExpressions.java
+++ src/ConditionalExpressions.java
@@ -22,8 +22,15 @@ public class ConditionalExpressions {
         boolean b4 = 10 >= 4;     // true
         boolean d4 = 10 >= 20;    // false
 
-        boolean c = 10 <= 10;    // true
-        boolean b = 10 <= 4;     // false
-        boolean d = 10 <= 20;    // true
+        boolean c5 = 10 <= 10;    // true
+        boolean b5 = 10 <= 4;     // false
+        boolean d5 = 10 <= 20;    // true
+
+        boolean a11 = (5 > 6) || (4 < 6); // 5 > 6 - false, 4 < 6 - true, поэтому возвращается true
+        boolean a12 = (5 > 6) || (4 > 6); // 5 > 6 - false, 4 > 6 - false, поэтому возвращается false
+        boolean a13 = (5 > 6) && (4 < 6); // 5 > 6 - false, поэтому возвращается false (4 < 6 - true, но не вычисляется)
+        boolean a14 = (50 > 6) && (4 / 2 < 3); // 50 > 6 - true, 4/2 < 3 - true, поэтому возвращается true
+        boolean a15 = (5 < 6) ^ (4 > 6); // 5 < 6 - true, поэтому возвращается true (4 > 6 - false)
+        boolean a16 = (50 > 6) ^ (4 / 2 < 3); // 50 > 6 - true, 4/2 < 3 - true, поэтому возвращается false
     }
 }
