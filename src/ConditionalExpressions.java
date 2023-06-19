d309f5ae453921d6f02ff542b360a8d2c38176d1
dannnik4 <daniilperstnev2001@gmail.com>
2023-06-19 21:46:11 +0300

    Added ConditionalExpressions.java

---
+++ src/ConditionalExpressions.java
@@ -0,0 +1,8 @@
+public class ConditionalExpressions {
+    public static void main(String args[]) {
+    int a = 10;
+    int b = 4;
+    boolean c = a == b;         // false
+    boolean d = a == 10;       // true
+    }
+}
