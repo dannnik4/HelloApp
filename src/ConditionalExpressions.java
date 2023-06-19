8e4f8d5d3052fff7468b8c935669809eb09dea34
dannnik4 <daniilperstnev2001@gmail.com>
2023-06-19 21:47:03 +0300

    Updated ConditionalExpressions.java

--- src/ConditionalExpressions.java
+++ src/ConditionalExpressions.java
@@ -1,8 +1,13 @@
 public class ConditionalExpressions {
     public static void main(String args[]) {
-    int a = 10;
-    int b = 4;
-    boolean c = a == b;         // false
-    boolean d = a == 10;       // true
+        int a = 10;
+        int b = 4;
+        boolean c = a == b;         // false
+        boolean d = a == 10;       // true
+
+        int a1 = 10;
+        int b1 = 4;
+        boolean c1 = a != b;         // true
+        boolean d1 = a != 10;       // false
     }
 }
