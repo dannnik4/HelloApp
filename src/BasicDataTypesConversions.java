304761f1faae9460270de9cef4008be3275e59ea
dannnik4 <daniilperstnev2001@gmail.com>
2023-06-21 10:14:55 +0300

    Updated BasicDataTypesConversions.java

--- src/BasicDataTypesConversions.java
+++ src/BasicDataTypesConversions.java
@@ -1,6 +1,10 @@
 public class BasicDataTypesConversions {
     public static void main(String args[]) {
+//        int a = 4;
+//        byte b = a; // ! Ошибка
+
         int a = 4;
-        byte b = a; // ! Ошибка
+        byte b = (byte)a;  // преобразование типов: от типа int к типу byte
+        System.out.println(b); // 4
     }
 }
