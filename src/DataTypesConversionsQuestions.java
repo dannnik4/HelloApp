421c1fa792cc3f06274e8c70baa8cc1353361773
dannnik4 <daniilperstnev2001@gmail.com>
2023-06-22 08:46:20 +0300

    Added DataTypesConversionsQuestions.java

---
+++ src/DataTypesConversionsQuestions.java
@@ -0,0 +1,8 @@
+public class DataTypesConversionsQuestions {
+    public static void main(String args[]) {
+        short shortNum = 257;
+        byte byteNum = (byte)shortNum;
+
+        System.out.println(byteNum);
+    }
+}
