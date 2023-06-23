22d69add67d840358ee504560e8380dd8489e6bc
Dannnik <daniilperstnev2001@gmail.com>
2023-06-23 16:08:01 +0300

    Added ConditionalConstructs.java

---
+++ src/ConditionalConstructs.java
@@ -0,0 +1,9 @@
+public class ConditionalConstructs {
+    public static void main(String args[]) {
+        int num1 = 6;
+        int num2 = 4;
+        if (num1 > num2) {
+            System.out.println("Первое число больше второго");
+        }
+    }
+}
