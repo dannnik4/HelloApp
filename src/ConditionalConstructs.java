957147e4e068d3adccdca6777a337e4750584a4f
Dannnik <daniilperstnev2001@gmail.com>
2023-06-23 16:17:32 +0300

    Updated ConditionalConstructs.java

--- src/ConditionalConstructs.java
+++ src/ConditionalConstructs.java
@@ -43,5 +43,24 @@ public class ConditionalConstructs {
             default:
                 System.out.println("число не равно 1, 8, 9");
         }
+
+        int output = 0;
+        switch(num){
+
+            case 1:
+                output = 3;
+                break;
+            case 2:
+            case 3:
+            case 4:
+                output = 6;
+                break;
+            case 5:
+                output = 12;
+                break;
+            default:
+                output = 24;
+        }
+        System.out.println(output);
     }
 }
