f812f3776050376a16c5151477c4dd30903793ad
Dannnik <daniilperstnev2001@gmail.com>
2023-06-23 16:13:46 +0300

    Updated ConditionalConstructs.java

--- src/ConditionalConstructs.java
+++ src/ConditionalConstructs.java
@@ -1,18 +1,26 @@
 public class ConditionalConstructs {
     public static void main(String args[]) {
-//        int num1 = 6;
-//        int num2 = 4;
-//        if (num1 > num2) {
-//            System.out.println("Первое число больше второго");
-//        }
-
         int num1 = 6;
         int num2 = 4;
+        if (num1 > num2) {
+            System.out.println("Первое число больше второго");
+        }
+
         if(num1>num2){
             System.out.println("Первое число больше второго");
         }
         else{
             System.out.println("Первое число меньше второго");
+        }
+
+        if(num1>num2){
+            System.out.println("Первое число больше второго");
+        }
+        else if(num1<num2){
+            System.out.println("Первое число меньше второго");
+        }
+        else{
+            System.out.println("Числа равны");
         }
     }
 }
