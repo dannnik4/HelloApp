cd3a1070896b7c3e642b305dfb53a28087b9162c
Dannnik <daniilperstnev2001@gmail.com>
2023-06-24 11:53:29 +0300

    Updated ConditionalConstructsQuestions.java

--- src/ConditionalConstructsQuestions.java
+++ src/ConditionalConstructsQuestions.java
@@ -1,7 +1,26 @@
 public class ConditionalConstructsQuestions {
     public static void main(String args[]) {
+//        int a = 5;
+//        if (a <= 0) if (a == 5) a++; else a--;
+//        System.out.println(a);
+
         int a = 5;
-        if (a <= 0) if (a == 5) a++; else a--;
+        switch(a){
+            case 4:
+                a++;
+            case 5:
+                a++;
+            case 6:
+            case 7:
+            case 8:
+                a++;
+                break;
+            case 9:
+                a++;
+                break;
+            default:
+                a++;
+        }
         System.out.println(a);
     }
 }
