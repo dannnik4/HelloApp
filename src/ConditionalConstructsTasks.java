c5a37d9c07e10e524b0aa8d2c3e770044ca2f994
Dannnik <daniilperstnev2001@gmail.com>
2023-06-26 19:47:35 +0300

    Updated ConditionalConstructsTasks.java

--- src/ConditionalConstructsTasks.java
+++ src/ConditionalConstructsTasks.java
@@ -46,5 +46,24 @@ public class ConditionalConstructsTasks {
             System.out.print("Ошибка");
         }
         in.close();
+
+        System.out.println("Введите номер операции: 1.Сложение  2.Вычитание  3.Умножение");
+        int operation = in.nextInt();
+
+        switch (operation)
+        {
+            case 1:
+                System.out.println("Сложение");
+                break;
+            case 2:
+                System.out.println("Вычитание");
+                break;
+            case 3:
+                System.out.println("Умножение");
+                break;
+            default:
+                System.out.println("Неизвестная операция");
+                break;
+        }
     }
 }
