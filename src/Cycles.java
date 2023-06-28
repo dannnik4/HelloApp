75ea6a17f0e094d94e1ab1564373eff328a7c00c
Dannnik <daniilperstnev2001@gmail.com>
2023-06-28 14:27:41 +0300

    Added Cycles.java

---
+++ src/Cycles.java
@@ -0,0 +1,8 @@
+public class Cycles {
+    public static void main(String args[]) {
+//        for ([инициализация счетчика]; [условие]; [изменение счетчика])
+//        {
+//            // действия
+//        }
+    }
+}
