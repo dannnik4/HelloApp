452713ade597b4032d21fe47436fa74c03a11a92
Dannnik <daniilperstnev2001@gmail.com>
2023-06-28 17:08:42 +0300

    Updated Cycles.java

--- src/Cycles.java
+++ src/Cycles.java
@@ -8,5 +8,10 @@ public class Cycles {
         for (int i = 1; i < 9; i++){
             System.out.printf("Квадрат числа %d равен %d \n", i, i * i);
         }
+
+//        int i = 1;
+//        for (; ;){
+//            System.out.printf("Квадрат числа %d равен %d \n", i, i * i);
+//        }
     }
 }
