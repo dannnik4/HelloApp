b2ba4ff1fb420173d8325f6790c616002c279722
Dannnik <daniilperstnev2001@gmail.com>
2023-06-30 00:08:01 +0300

    Updated CyclesQuestions.java

--- src/CyclesQuestions.java
+++ src/CyclesQuestions.java
@@ -1,10 +1,20 @@
 public class CyclesQuestions {
     public static void main(String args[]) {
-        int i = 5;
-        while(i > 0)
+//        int i = 5;
+//        while(i > 0)
+//        {
+//            i *= 3;
+//            i *= -2;
+//        }
+
+        int j = 2;
+        for (int i = 1; i < 100; i = i + 2)
         {
-            i *= 3;
-            i *= -2;
+            j = j - 1;
+            while(j < 15)
+            {
+                j = j + 5;
+            }
         }
     }
 }
