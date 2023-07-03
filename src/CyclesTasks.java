794439b8f0793be7cd85b246ec384c2b2e12ebe6
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-03 21:22:17 +0300

    Updated CyclesTasks.java

--- src/CyclesTasks.java
+++ src/CyclesTasks.java
@@ -14,7 +14,7 @@ public class CyclesTasks {
         int death = 8;
         for(int i = 1; i <= 10; i++){
             if (born > 7) born -= 1;
-            if  (death > 6) death -=1;
+            if (death > 6) death -=1;
             population += population * (born - death) / 1000;
         }
         System.out.println(population);
