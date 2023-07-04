e9cec2d1c5089ce9958fb303b3f41ea05b7583e6
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-04 23:54:18 +0300

    Updated CyclesTasks.java

--- src/CyclesTasks.java
+++ src/CyclesTasks.java
@@ -46,5 +46,13 @@ public class CyclesTasks {
 //        }
 //        System.out.print("После " + months + " месяцев сумма вклада составит " + deposit);
 //        in.close();
+
+
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                System.out.printf("%d\t", i * j);
+            }
+            System.out.println();
+        }
     }
 }
