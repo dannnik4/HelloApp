0aa9fc04d3f6682bed474f1e76ca53b7c886524a
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-05 21:35:39 +0300

    Updated Arrays.java

--- src/Arrays.java
+++ src/Arrays.java
@@ -9,7 +9,12 @@ public class Arrays {
 //
 //        nums = new int[4];  // массив из 4 чисел
 
-        int nums[] = new int[4];    // массив из 4 чисел
-        int[] nums2 = new int[5];   // массив из 5 чисел
+//        int nums[] = new int[4];    // массив из 4 чисел
+//        int[] nums2 = new int[5];   // массив из 5 чисел
+
+        // эти два способа равноценны
+        int[] nums = new int[] { 1, 2, 3, 5 };
+
+        int[] nums2 = { 1, 2, 3, 5 };
     }
 }
