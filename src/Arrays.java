5e55ae533acdf41612da75d0fc58121bb10d3440
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-05 21:35:10 +0300

    Updated Arrays.java

--- src/Arrays.java
+++ src/Arrays.java
@@ -4,9 +4,12 @@ public class Arrays {
 //        ибо
 //        тип_данных[] название_массива;
 
-        int nums[];
+//        int nums[];
 //        int[] nums2;
+//
+//        nums = new int[4];  // массив из 4 чисел
 
-        nums = new int[4];  // массив из 4 чисел
+        int nums[] = new int[4];    // массив из 4 чисел
+        int[] nums2 = new int[5];   // массив из 5 чисел
     }
 }
