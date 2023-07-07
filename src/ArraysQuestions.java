fab65273c102b13251fabbeaeb271ddfa5f38cee
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-07 23:38:00 +0300

    Updated ArraysQuestions.java

--- src/ArraysQuestions.java
+++ src/ArraysQuestions.java
@@ -2,13 +2,15 @@ public class ArraysQuestions {
     public static void main(String args[]) {
 //        int[][][] nums = new int[3][2][3];
 
-        int[][] nums =
-                {
-                        {1, 2},
-                        {3, 4},
-                        {5, 6}
-                };
+//        int[][] nums =
+//                {
+//                        {1, 2},
+//                        {3, 4},
+//                        {5, 6}
+//                };
+//
+//        System.out.println(nums[3][2]);
 
-        System.out.println(nums[3][2]);
+        int[][] nums = {{ 1, 2 }, { 3, 4, 5 }, { 6, 7, 8, 9, 10 }};
     }
 }
