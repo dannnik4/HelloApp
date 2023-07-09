d0052c7e9852e7199a8bed41a9b32608d71ed78a
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-09 10:38:55 +0300

    Updated MethodsOptions.java

--- src/MethodsOptions.java
+++ src/MethodsOptions.java
@@ -1,17 +1,29 @@
 public class MethodsOptions {
-    public static void main(String args[]) {
+//    public static void main(String args[]) {
+//
+//        int a = 6;
+//        int b = 8;
+//        sum(a, b);  // 14
+//        sum(3, a);  // 9
+//        sum(5, 23); // 28
+//    }
+//
+//    static void sum(int x, int y) {
+//
+//        int z = x + y;
+//        System.out.println(z);
+//    }
 
-        int a = 6;
-        int b = 8;
-        sum(a, b);  // 14
-        sum(3, a);  // 9
-        sum(5, 23); // 28
+    public static void main (String args[]){
+
+        display("Tom", 34);
+        display("Bob", 28);
+        display("Sam", 23);
     }
+    static void display(String name, int age){
 
-    static void sum(int x, int y) {
-
-        int z = x + y;
-        System.out.println(z);
+        System.out.println(name);
+        System.out.println(age);
     }
 
 }
