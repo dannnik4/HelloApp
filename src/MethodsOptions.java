b739c6591d2505a82508728c4779ebb2a4f7ab87
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-09 10:37:38 +0300

    Updated MethodsOptions.java

--- src/MethodsOptions.java
+++ src/MethodsOptions.java
@@ -1,6 +1,11 @@
 public class MethodsOptions {
     public static void main(String args[]) {
 
+        int a = 6;
+        int b = 8;
+        sum(a, b);  // 14
+        sum(3, a);  // 9
+        sum(5, 23); // 28
     }
 
     static void sum(int x, int y) {
