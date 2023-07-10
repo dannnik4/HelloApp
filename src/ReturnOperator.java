d75d8408cbc23cf6c5ba6faacd824b44a7c041f6
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-10 18:53:49 +0300

    Updated ReturnOperator.java

--- src/ReturnOperator.java
+++ src/ReturnOperator.java
@@ -1,15 +1,36 @@
 public class ReturnOperator {
+//    public static void main (String args[]){
+//
+//        return возвращаемое_значение;
+//
+//        int x = sum(1, 2, 3);
+//        int y = sum(1, 4, 9);
+//        System.out.println(x);  // 6
+//        System.out.println(y);  // 14
+//    }
+//    static int sum(int a, int b, int c){
+//
+//        return a + b + c;
+//    }
+
     public static void main (String args[]){
 
-//        return возвращаемое_значение;
-
-        int x = sum(1, 2, 3);
-        int y = sum(1, 4, 9);
-        System.out.println(x);  // 6
-        System.out.println(y);  // 14
+        System.out.println(daytime(7));     // Good morning
+        System.out.println(daytime(13));    // Good after noon
+        System.out.println(daytime(18));    // Good evening
+        System.out.println(daytime(2));     // Good night
     }
-    static int sum(int a, int b, int c){
+    static String daytime(int hour){
 
-        return a + b + c;
+        if (hour >24 || hour < 0)
+            return "Invalid data";
+        else if(hour > 21 || hour < 6)
+            return "Good night";
+        else if(hour >= 15)
+            return "Good evening";
+        else if(hour >= 11)
+            return "Good after noon";
+        else
+            return "Good morning";
     }
 }
