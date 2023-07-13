e7c0fc0ca18e741b5a8e2aef5b2a920383acad18
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-13 10:35:01 +0300

    Updated ExceptionsHandling.java

--- src/ExceptionsHandling.java
+++ src/ExceptionsHandling.java
@@ -1,5 +1,16 @@
 public class ExceptionsHandling {
     public static void main(String[] args) {
+//        try{
+//            int[] numbers = new int[3];
+//            numbers[4]=45;
+//            System.out.println(numbers[4]);
+//        }
+//        catch(Exception ex){
+//
+//            ex.printStackTrace();
+//        }
+//        System.out.println("Программа завершена");
+
         try{
             int[] numbers = new int[3];
             numbers[4]=45;
@@ -8,6 +19,9 @@ public class ExceptionsHandling {
         catch(Exception ex){
 
             ex.printStackTrace();
+        }
+        finally{
+            System.out.println("Блок finally");
         }
         System.out.println("Программа завершена");
     }
