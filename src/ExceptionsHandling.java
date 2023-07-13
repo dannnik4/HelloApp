711431da2e3f7426f401aad6b293e6d22708aad9
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-13 10:33:39 +0300

    Added ExceptionsHandling.java

---
+++ src/ExceptionsHandling.java
@@ -0,0 +1,14 @@
+public class ExceptionsHandling {
+    public static void main(String[] args) {
+        try{
+            int[] numbers = new int[3];
+            numbers[4]=45;
+            System.out.println(numbers[4]);
+        }
+        catch(Exception ex){
+
+            ex.printStackTrace();
+        }
+        System.out.println("Программа завершена");
+    }
+}
