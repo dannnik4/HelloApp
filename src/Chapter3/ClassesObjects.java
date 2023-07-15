20959a40276489fdcb874d7de54e78659035ea32
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-15 11:57:48 +0300

    Updated ClassesObjects.java

--- src/Chapter3/ClassesObjects.java
+++ src/Chapter3/ClassesObjects.java
@@ -1,8 +1,24 @@
 package Chapter3;
 
 class ClassesObjects {
-    String name;        // имя
-    int age;            // возраст
+//    String name;        // имя
+//    int age;            // возраст
+//
+//    void displayInfo() {
+//        System.out.printf("Name: %s \tAge: %d\n", name, age);
+//    }
+
+
+    public static void main(String[] args) {
+
+        Person tom;
+    }
+}
+
+class Person {
+
+    String name;    // имя
+    int age;        // возраст
 
     void displayInfo() {
         System.out.printf("Name: %s \tAge: %d\n", name, age);
