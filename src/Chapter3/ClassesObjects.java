b82142810aae03f751a0f237d2f8e8cb32905f35
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-15 11:59:16 +0300

    Updated ClassesObjects.java

--- src/Chapter3/ClassesObjects.java
+++ src/Chapter3/ClassesObjects.java
@@ -8,19 +8,37 @@ class ClassesObjects {
 //        System.out.printf("Name: %s \tAge: %d\n", name, age);
 //    }
 
+//    public static void main(String[] args) {
+//
+//        Person tom;
+//    }
+//}
+//
+//class Person {
+//
+//    String name;    // имя
+//    int age;        // возраст
+//
+//    void displayInfo() {
+//        System.out.printf("Name: %s \tAge: %d\n", name, age);
+//    }
 
     public static void main(String[] args) {
 
-        Person tom;
+        Person tom = new Person(); // создание объекта
+        tom.displayInfo();
+
+        // изменяем имя и возраст
+        tom.name = "Tom";
+        tom.age = 34;
+        tom.displayInfo();
     }
 }
-
-class Person {
+class Person{
 
     String name;    // имя
     int age;        // возраст
-
-    void displayInfo() {
+    void displayInfo(){
         System.out.printf("Name: %s \tAge: %d\n", name, age);
     }
 }
