da7d59959e8657455346731ef22ab68285095a92
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-15 11:56:40 +0300

    Added ClassesObjects.java

---
+++ src/Chapter3/ClassesObjects.java
@@ -0,0 +1,10 @@
+package Chapter3;
+
+class ClassesObjects {
+    String name;        // имя
+    int age;            // возраст
+
+    void displayInfo() {
+        System.out.printf("Name: %s \tAge: %d\n", name, age);
+    }
+}
