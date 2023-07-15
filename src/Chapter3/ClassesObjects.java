e7b03380336d508492b18c3b81e43cf66449bc5c
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-15 12:00:58 +0300

    Updated ClassesObjects.java

--- src/Chapter3/ClassesObjects.java
+++ src/Chapter3/ClassesObjects.java
@@ -42,15 +42,50 @@ class ClassesObjects {
 //        System.out.printf("Name: %s \tAge: %d\n", name, age);
 //    }
 
+//    public static void main(String[] args) {
+//
+//        Person bob = new Person();      // вызов первого конструктора без параметров
+//        bob.displayInfo();
+//
+//        Person tom = new Person("Tom"); // вызов второго конструктора с одним параметром
+//        tom.displayInfo();
+//
+//        Person sam = new Person("Sam", 25); // вызов третьего конструктора с двумя параметрами
+//        sam.displayInfo();
+//    }
+//}
+//class Person{
+//
+//    String name;    // имя
+//    int age;        // возраст
+//    Person()
+//    {
+//        name = "Undefined";
+//        age = 18;
+//    }
+//    Person(String n)
+//    {
+//        name = n;
+//        age = 18;
+//    }
+//    Person(String n, int a)
+//    {
+//        name = n;
+//        age = a;
+//    }
+//    void displayInfo(){
+//        System.out.printf("Name: %s \tAge: %d\n", name, age);
+//    }
+
     public static void main(String[] args) {
 
-        Person bob = new Person();      // вызов первого конструктора без параметров
-        bob.displayInfo();
+        Person undef = new Person();
+        undef.displayInfo();
 
-        Person tom = new Person("Tom"); // вызов второго конструктора с одним параметром
+        Person tom = new Person("Tom");
         tom.displayInfo();
 
-        Person sam = new Person("Sam", 25); // вызов третьего конструктора с двумя параметрами
+        Person sam = new Person("Sam", 25);
         sam.displayInfo();
     }
 }
@@ -60,18 +95,16 @@ class Person{
     int age;        // возраст
     Person()
     {
-        name = "Undefined";
-        age = 18;
+        this("Undefined", 18);
     }
-    Person(String n)
+    Person(String name)
     {
-        name = n;
-        age = 18;
+        this(name, 18);
     }
-    Person(String n, int a)
+    Person(String name, int age)
     {
-        name = n;
-        age = a;
+        this.name = name;
+        this.age = age;
     }
     void displayInfo(){
         System.out.printf("Name: %s \tAge: %d\n", name, age);
