4e8e8086fdc8f51b3fb73e7437db10586ded5b14
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-16 11:47:49 +0300

    Updated Packages.java

--- src/Chapter3/Packages.java
+++ src/Chapter3/Packages.java
@@ -1,24 +1,32 @@
 package Chapter3;
 
+import java.util.Scanner; // импорт класса Scanner
+
 public class Packages {
+//    public static void main(String[] args) {
+//
+//        Person kate = new Person("Kate", 32);
+//        kate.displayInfo();
+//    }
+//}
+
+//class Person1 {
+//
+//    String name;
+//    int age;
+//
+//    Person1(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//    }
+//
+//    void displayInfo() {
+//        System.out.printf("Name: %s \t Age: %d \n", name, age);
+//    }
+
     public static void main(String[] args) {
 
         Person kate = new Person("Kate", 32);
         kate.displayInfo();
-    }
-}
-
-class Person1 {
-
-    String name;
-    int age;
-
-    Person1(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    void displayInfo() {
-        System.out.printf("Name: %s \t Age: %d \n", name, age);
     }
 }
