7fea23d3981556695ac5d3bca2699094c6f88c12
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-19 23:48:35 +0300

    Updated ObjectsasMethodParameters.java

--- src/Chapter3/ObjectsasMethodParameters.java
+++ src/Chapter3/ObjectsasMethodParameters.java
@@ -3,10 +3,45 @@ package Chapter3;
 public class ObjectsasMethodParameters {
     public static void main(String[] args) {
 
+//        Person2 kate = new Person2("Kate");
+//        System.out.println(kate.getName());     // Kate
+//        changeName(kate);
+//        System.out.println(kate.getName());     // Alice
+//    }
+//
+//    static void changeName(Person2 p) {
+//        p.setName("Alice");
+//    }
+//}
+//
+//class Person2 {
+//
+//    private String name;
+//
+//    Person2(String name) {
+//        this.name = name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getName() {
+//
+//        return this.name;
+//    }
+//}
+
         Person2 kate = new Person2("Kate");
         System.out.println(kate.getName());     // Kate
-        changeName(kate);
-        System.out.println(kate.getName());     // Alice
+        changePerson(kate);
+        System.out.println(kate.getName());     // Kate - изменения не произошло
+        // kate хранит ссылку на старый объект
+    }
+
+    static void changePerson(Person2 p) {
+        p = new Person2("Alice");    // p указывает на новый объект
+        p.setName("Ann");
     }
 
     static void changeName(Person2 p) {
