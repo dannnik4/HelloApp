a2be3a4c8122661267918001ab5a8ba8625e34f8
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-20 23:53:34 +0300

    Updated InnerAndNestedClasses.java

--- src/Chapter3/InnerAndNestedClasses.java
+++ src/Chapter3/InnerAndNestedClasses.java
@@ -2,32 +2,60 @@ package Chapter3;
 
 public class InnerAndNestedClasses {
     public static void main(String[] args) {
-        Person3 tom = new Person3("Tom", "qwerty");
-        tom.displayPerson();
-        tom.account.displayAccount();
+//        Person3 tom = new Person3("Tom", "qwerty");
+//        tom.displayPerson();
+//        tom.account.displayAccount();
+//    }
+//}
+//
+//class Person3 {
+//
+//    private String name;
+//    Account account;
+//
+//    Person3(String name, String password) {
+//        this.name = name;
+//        account = new Account(password);
+//    }
+//
+//    public void displayPerson() {
+//        System.out.printf("Person \t Name: %s \t Password: %s \n", name, account.password);
+//    }
+//
+//    public class Account {
+//        private String password;
+//
+//        Account(String pass) {
+//            this.password = pass;
+//        }
+//
+//        void displayAccount() {
+//            System.out.printf("Account Login: %s \t Password: %s \n", Person.this.name, password);
+//        }
+//    }
+//}
+
+        Person3 tom = new Person3("Tom");
+        tom.setAccount("qwerty");
     }
 }
 class Person3{
 
     private String name;
-    Account account;
 
-    Person3(String name, String password){
+    Person3(String name){
         this.name = name;
-        account = new Account(password);
-    }
-    public void displayPerson(){
-        System.out.printf("Person \t Name: %s \t Password: %s \n", name, account.password);
     }
 
-    public class Account{
-        private String password;
+    public void setAccount (String password){
 
-        Account(String pass){
-            this.password = pass;
+        class Account{
+
+            void display(){
+                System.out.printf("Account Login: %s \t Password: %s \n", name, password);
+            }
         }
-        void displayAccount(){
-            System.out.printf("Account Login: %s \t Password: %s \n", Person.this.name, password);
-        }
+        Account account = new Account();
+        account.display();
     }
 }
