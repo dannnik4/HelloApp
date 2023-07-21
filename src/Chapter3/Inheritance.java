fca4a27580897a3b91194ded41142ed283d4843a
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-21 23:36:33 +0300

    Updated Inheritance.java

--- src/Chapter3/Inheritance.java
+++ src/Chapter3/Inheritance.java
@@ -2,26 +2,64 @@ package Chapter3;
 
 public class Inheritance {
     public static void main(String[] args) {
-        class Person3 {
+//        class Person3 {
+//
+//            String name;
+//            public String getName(){ return name; }
+//
+//            public Person3(String name){
+//
+//                this.name=name;
+//            }
+//
+//            public void display(){
+//
+//                System.out.println("Name: " + name);
+//            }
+//        }
+//        class Employee extends Person3{
+//            public Employee(String name){
+//                super(name);    // если базовый класс определяет конструктор
+//                //  то производный класс должен его вызвать
+//            }
+//        }
 
-            String name;
-            public String getName(){ return name; }
+        Employee sam = new Employee("Sam", "Microsoft");
+        sam.display();  // Sam
+        sam.work();     // Sam works in Microsoft
+    }
+}
 
-            public Person3(String name){
+class Person3 {
 
-                this.name=name;
-            }
+    String name;
 
-            public void display(){
+    public String getName() {
+        return name;
+    }
 
-                System.out.println("Name: " + name);
-            }
-        }
-        class Employee extends Person3{
-            public Employee(String name){
-                super(name);    // если базовый класс определяет конструктор
-                //  то производный класс должен его вызвать
-            }
-        }
+    public Person3(String name) {
+
+        this.name = name;
+    }
+
+    public void display() {
+
+        System.out.println("Name: " + name);
+    }
+}
+
+class Employee extends Person3 {
+
+    String company;
+
+    public Employee(String name, String company) {
+
+        super(name);
+        this.company = company;
+    }
+
+    public void work() {
+        System.out.printf("%s works in %s \n", getName(), company);
     }
 }
