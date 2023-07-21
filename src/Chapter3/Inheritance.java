208450fb7f13f9670ca6dcac3bd1399273caf4a4
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-21 23:34:02 +0300

    Add Inheritance.java

---
+++ src/Chapter3/Inheritance.java
@@ -0,0 +1,21 @@
+package Chapter3;
+
+public class Inheritance {
+    public static void main(String[] args) {
+        class Person3 {
+
+            String name;
+            public String getName(){ return name; }
+
+            public Person3(String name){
+
+                this.name=name;
+            }
+
+            public void display(){
+
+                System.out.println("Name: " + name);
+            }
+        }
+    }
+}
