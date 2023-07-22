cbccbf478aa0beb8cde6edea68c916ec609be19c
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-22 16:36:04 +0300

    Added AbstractClasses.java

---
+++ src/Chapter3/AbstractClasses.java
@@ -0,0 +1,12 @@
+package Chapter3;
+
+public class AbstractClasses {
+    public static void main(String[] args) {
+        abstract class Human{
+
+            private String name;
+
+            public String getName() { return name; }
+        }
+    }
+}
