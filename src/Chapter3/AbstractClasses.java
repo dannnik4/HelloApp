af1e37f527ef40824e347b1b0d74ddbdadffbf36
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-22 16:39:01 +0300

    Updated AbstractClasses.java

--- src/Chapter3/AbstractClasses.java
+++ src/Chapter3/AbstractClasses.java
@@ -11,54 +11,98 @@ public class AbstractClasses {
 //    }
 //}
 
-        Employee1 sam = new Employee1("Sam", "Leman Brothers");
-        sam.display();
-        Client bob = new Client("Bob", "Leman Brothers");
-        bob.display();
-    }
-}
-abstract class Person4 {
+//        Employee1 sam = new Employee1("Sam", "Leman Brothers");
+//        sam.display();
+//        Client bob = new Client("Bob", "Leman Brothers");
+//        bob.display();
+//    }
+//}
+//abstract class Person4 {
+//
+//    private String name;
+//
+//    public String getName() { return name; }
+//
+//    public Person4(String name){
+//
+//        this.name=name;
+//    }
+//
+//    public abstract void display();
+//}
+//
+//class Employee1 extends Person4{
+//
+//    private String bank;
+//
+//    public Employee1(String name, String company) {
+//
+//        super(name);
+//        this.bank = company;
+//    }
+//
+//    public void display(){
+//
+//        System.out.printf("Employee Name: %s \t Bank: %s \n", super.getName(), bank);
+//    }
+//}
+//
+//class Client extends Person4
+//{
+//    private String bank;
+//
+//    public Client(String name, String company) {
+//
+//        super(name);
+//        this.bank = company;
+//    }
+//
+//    public void display(){
+//
+//        System.out.printf("Client Name: %s \t Bank: %s \n", super.getName(), bank);
+//    }
+//}
 
-    private String name;
+        // абстрактный класс фигуры
+        abstract class Figure {
 
-    public String getName() { return name; }
+            float x; // x-координата точки
+            float y; // y-координата точки
 
-    public Person4(String name){
+            Figure(float x, float y) {
 
-        this.name=name;
-    }
+                this.x = x;
+                this.y = y;
+            }
 
-    public abstract void display();
-}
+            // абстрактный метод для получения периметра
+            public abstract float getPerimeter();
 
-class Employee1 extends Person4{
+            // абстрактный метод для получения площади
+            public abstract float getArea();
+        }
+        // производный класс прямоугольника
+        class Rectangle extends Figure {
+            private float width;
+            private float height;
 
-    private String bank;
+            // конструктор с обращением к конструктору класса Figure
+            Rectangle(float x, float y, float width, float height) {
 
-    public Employee1(String name, String company) {
+                super(x, y);
+                this.width = width;
+                this.height = height;
+            }
 
-        super(name);
-        this.bank = company;
-    }
+            public float getPerimeter() {
 
-    public void display(){
+                return width * 2 + height * 2;
+            }
 
-        System.out.printf("Employee Name: %s \t Bank: %s \n", super.getName(), bank);
-    }
-}
+            public float getArea() {
 
-class Client extends Person4
-{
-    private String bank;
-
-    public Client(String name, String company) {
-
-        super(name);
-        this.bank = company;
-    }
-
-    public void display(){
-
-        System.out.printf("Client Name: %s \t Bank: %s \n", super.getName(), bank);
+                return width * height;
+            }
+        }
     }
 }
