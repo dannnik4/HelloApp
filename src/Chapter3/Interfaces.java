a88ab61dd264f401c68e7c5fd8297b39facd0aff
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-24 23:22:17 +0300

    Updated Interfaces.java

--- src/Chapter3/Interfaces.java
+++ src/Chapter3/Interfaces.java
@@ -2,9 +2,34 @@ package Chapter3;
 
 public class Interfaces {
     public static void main(String[] args) {
-        interface Printable{
+//        interface Printable{
+//
+//            void print();
+//        }
+//    }
+//}
 
-            void print();
-        }
+        Book b1 = new Book("Java. Complete Referense.", "H. Shildt");
+        b1.print();
+    }
+}
+interface Printable{
+
+    void print();
+}
+class Book implements Printable{
+
+    String name;
+    String author;
+
+    Book(String name, String author){
+
+        this.name = name;
+        this.author = author;
+    }
+
+    public void print() {
+
+        System.out.printf("%s (%s) \n", name, author);
     }
 }
