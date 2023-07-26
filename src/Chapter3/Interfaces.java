3de71851955a0347c8701ddf234460a2123fce67
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-26 23:18:19 +0300

    Updated Interfaces.java

--- src/Chapter3/Interfaces.java
+++ src/Chapter3/Interfaces.java
@@ -69,32 +69,55 @@ public class Interfaces {
 //    }
 //}
 
-        Calculatable c = new Calculation();
-        System.out.println(c.sum(1, 2));
-        System.out.println(c.sum(1, 2, 4));
+//        Calculatable c = new Calculation();
+//        System.out.println(c.sum(1, 2));
+//        System.out.println(c.sum(1, 2, 4));
+//    }
+//}
+//class Calculation implements Calculatable{
+//
+//}
+//interface Calculatable{
+//
+//    default int sum(int a, int b){
+//        return sumAll(a, b);
+//    }
+//    default int sum(int a, int b, int c){
+//        return sumAll(a, b, c);
+//    }
+//
+//    private int sumAll(int... values){
+//        int result = 0;
+//        for(int n : values){
+//            result += n;
+//        }
+//        return result;
+//    }
+//}
+//
+//interface Stateable{
+//
+//    int OPEN = 1;
+//    int CLOSED = 0;
+//
+//    void printState(int n);
+//}
+
+        WaterPipe pipe = new WaterPipe();
+        pipe.printState(1);
     }
 }
-class Calculation implements Calculatable{
+class WaterPipe implements Stateable{
 
-}
-interface Calculatable{
-
-    default int sum(int a, int b){
-        return sumAll(a, b);
-    }
-    default int sum(int a, int b, int c){
-        return sumAll(a, b, c);
-    }
-
-    private int sumAll(int... values){
-        int result = 0;
-        for(int n : values){
-            result += n;
-        }
-        return result;
+    public void printState(int n){
+        if(n==OPEN)
+            System.out.println("Water is opened");
+        else if(n==CLOSED)
+            System.out.println("Water is closed");
+        else
+            System.out.println("State is invalid");
     }
 }
-
 interface Stateable{
 
     int OPEN = 1;
