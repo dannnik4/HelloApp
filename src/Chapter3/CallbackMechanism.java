7e3f605209a0a43d64ad2961681ba916d19aa4cd
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-27 09:50:13 +0300

    Updated CallbackMechanism.java

--- src/Chapter3/CallbackMechanism.java
+++ src/Chapter3/CallbackMechanism.java
@@ -39,32 +39,32 @@ public class CallbackMechanism {
 //    }
 //}
 
-        Button tvButton = new Button(new EventHandler(){
-
-            private boolean on = false;
-            public void execute(){
-
-                if(on) {
-                    System.out.println("Телевизор выключен..");
-                    on=false;
-                }
-                else {
-                    System.out.println("Телевизор включен!");
-                    on=true;
-                }
-            }
-        });
-
-        Button printButton = new Button(new EventHandler(){
-
-            public void execute(){
-
-                System.out.println("Запущена печать на принтере...");
-            }
-        });
-
-        tvButton.click();
-        printButton.click();
-        tvButton.click();
+//        Button tvButton = new Button(new EventHandler(){
+//
+//            private boolean on = false;
+//            public void execute(){
+//
+//                if(on) {
+//                    System.out.println("Телевизор выключен..");
+//                    on=false;
+//                }
+//                else {
+//                    System.out.println("Телевизор включен!");
+//                    on=true;
+//                }
+//            }
+//        });
+//
+//        Button printButton = new Button(new EventHandler(){
+//
+//            public void execute(){
+//
+//                System.out.println("Запущена печать на принтере...");
+//            }
+//        });
+//
+//        tvButton.click();
+//        printButton.click();
+//        tvButton.click();
     }
 }
