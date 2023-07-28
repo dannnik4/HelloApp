7c356e885d816323538eeb7dcc39c13afb92644e
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-28 19:11:50 +0300

    Updated EnumerationEnum.java

--- src/Chapter3/EnumerationEnum.java
+++ src/Chapter3/EnumerationEnum.java
@@ -17,39 +17,51 @@ public class EnumerationEnum {
 //    SUNDAY
 //}
 
-        Book1 b1 = new Book1("War and Peace", "L. Tolstoy", Type.BELLETRE);
-        System.out.printf("Book '%s' has a type %s", b1.name, b1.bookType);
+//        Book1 b1 = new Book1("War and Peace", "L. Tolstoy", Type.BELLETRE);
+//        System.out.printf("Book '%s' has a type %s", b1.name, b1.bookType);
+//
+//        switch(b1.bookType){
+//            case BELLETRE:
+//                System.out.println("Belletre");
+//                break;
+//            case SCIENCE:
+//                System.out.println("Science");
+//                break;
+//            case SCIENCE_FICTION:
+//                System.out.println("Science fiction");
+//                break;
+//            case PHANTASY:
+//                System.out.println("Phantasy");
+//                break;
+//        }
+//    }
+//}
+//class Book1{
+//
+//    String name;
+//    Type bookType;
+//    String author;
+//
+//    Book1(String name, String author, Type type){
+//
+//        bookType = type;
+//        this.name = name;
+//        this.author = author;
+//    }
+//}
+//
+//enum Type
+//{
+//    SCIENCE,
+//    BELLETRE,
+//    PHANTASY,
+//    SCIENCE_FICTION
+//}
 
-        switch(b1.bookType){
-            case BELLETRE:
-                System.out.println("Belletre");
-                break;
-            case SCIENCE:
-                System.out.println("Science");
-                break;
-            case SCIENCE_FICTION:
-                System.out.println("Science fiction");
-                break;
-            case PHANTASY:
-                System.out.println("Phantasy");
-                break;
-        }
+        Type[] types = Type.values();
+        for (Type s : types) { System.out.println(s); }
     }
 }
-class Book1{
-
-    String name;
-    Type bookType;
-    String author;
-
-    Book1(String name, String author, Type type){
-
-        bookType = type;
-        this.name = name;
-        this.author = author;
-    }
-}
-
 enum Type
 {
     SCIENCE,
