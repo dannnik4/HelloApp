81bbcea654a91e2d6f2d0656062e3eb1a8d25c57
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-28 19:11:29 +0300

    Updated EnumerationEnum.java

--- src/Chapter3/EnumerationEnum.java
+++ src/Chapter3/EnumerationEnum.java
@@ -2,17 +2,58 @@ package Chapter3;
 
 public class EnumerationEnum {
     public static void main(String[] args) {
-        Day current = Day.MONDAY;
-        System.out.println(current);    // MONDAY
+//        Day current = Day.MONDAY;
+//        System.out.println(current);    // MONDAY
+//    }
+//}
+//enum Day{
+//
+//    MONDAY,
+//    TUESDAY,
+//    WEDNESDAY,
+//    THURSDAY,
+//    FRIDAY,
+//    SATURDAY,
+//    SUNDAY
+//}
+
+        Book1 b1 = new Book1("War and Peace", "L. Tolstoy", Type.BELLETRE);
+        System.out.printf("Book '%s' has a type %s", b1.name, b1.bookType);
+
+        switch(b1.bookType){
+            case BELLETRE:
+                System.out.println("Belletre");
+                break;
+            case SCIENCE:
+                System.out.println("Science");
+                break;
+            case SCIENCE_FICTION:
+                System.out.println("Science fiction");
+                break;
+            case PHANTASY:
+                System.out.println("Phantasy");
+                break;
+        }
     }
 }
-enum Day{
+class Book1{
 
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY
+    String name;
+    Type bookType;
+    String author;
+
+    Book1(String name, String author, Type type){
+
+        bookType = type;
+        this.name = name;
+        this.author = author;
+    }
+}
+
+enum Type
+{
+    SCIENCE,
+    BELLETRE,
+    PHANTASY,
+    SCIENCE_FICTION
 }
