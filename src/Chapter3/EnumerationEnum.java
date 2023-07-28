d89533e5f950f3027b2f8f218d104f12afd9dce2
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-28 19:10:40 +0300

    Updated EnumerationEnum.java

--- src/Chapter3/EnumerationEnum.java
+++ src/Chapter3/EnumerationEnum.java
@@ -2,15 +2,17 @@ package Chapter3;
 
 public class EnumerationEnum {
     public static void main(String[] args) {
-        enum Day{
-
-            MONDAY,
-            TUESDAY,
-            WEDNESDAY,
-            THURSDAY,
-            FRIDAY,
-            SATURDAY,
-            SUNDAY
-        }
+        Day current = Day.MONDAY;
+        System.out.println(current);    // MONDAY
     }
+}
+enum Day{
+
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
 }
