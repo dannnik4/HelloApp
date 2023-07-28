e7c32ff47ca8db2a9321c3ef8fea37c3f781893d
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-28 19:12:40 +0300

    Updated EnumerationEnum.java

--- src/Chapter3/EnumerationEnum.java
+++ src/Chapter3/EnumerationEnum.java
@@ -70,17 +70,36 @@ public class EnumerationEnum {
 //    SCIENCE_FICTION
 //}
 
-        System.out.println(Color.RED.getCode());        // #FF0000
-        System.out.println(Color.GREEN.getCode());      // #00FF00
+//        System.out.println(Color.RED.getCode());        // #FF0000
+//        System.out.println(Color.GREEN.getCode());      // #00FF00
+//
+//
+//    }
+//}
+//enum Color{
+//    RED("#FF0000"), BLUE("#0000FF"), GREEN("#00FF00");
+//    private String code;
+//    Color(String code){
+//        this.code = code;
+//    }
+//    public String getCode(){ return code;}
+//}
 
-
+        Operation op = Operation.SUM;
+        System.out.println(op.action(10, 4));   // 14
+        op = Operation.MULTIPLY;
+        System.out.println(op.action(6, 4));    // 24
     }
 }
-enum Color{
-    RED("#FF0000"), BLUE("#0000FF"), GREEN("#00FF00");
-    private String code;
-    Color(String code){
-        this.code = code;
-    }
-    public String getCode(){ return code;}
+enum Operation{
+    SUM{
+        public int action(int x, int y){ return x + y;}
+    },
+    SUBTRACT{
+        public int action(int x, int y){ return x - y;}
+    },
+    MULTIPLY{
+        public int action(int x, int y){ return x * y;}
+    };
+    public abstract int action(int x, int y);
 }
