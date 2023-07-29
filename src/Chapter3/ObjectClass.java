5616d691ddb1ab6f1476a77b5abe1188ccd95150
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-29 12:07:52 +0300

    Added ObjectClass.java

--- src/Chapter3/ObjectClass.java
+++ src/Chapter3/ObjectClass.java
@@ -3,7 +3,8 @@ package Chapter3;
 public class ObjectClass {
     public static void main(String[] args) {
         Person4 tom = new Person4("Tom");
-        System.out.println(tom.toString()); // Будет выводить что-то наподобие Person@7960847b
+//        System.out.println(tom.toString()); // Будет выводить что-то наподобие Person@7960847b
+        System.out.println(tom.toString()); // Person Tom
     }
 }
 class Person4 {
@@ -13,5 +14,11 @@ class Person4 {
     public Person4(String name){
 
         this.name=name;
+    }
+
+    @Override
+    public String toString(){
+
+        return "Person " + name;
     }
 }
