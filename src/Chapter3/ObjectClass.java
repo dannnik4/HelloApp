e0bb80377353b96a497407ed2d3dfcdfabe30b5d
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-29 12:10:25 +0300

    Updated ObjectClass.java

--- src/Chapter3/ObjectClass.java
+++ src/Chapter3/ObjectClass.java
@@ -5,7 +5,8 @@ public class ObjectClass {
         Person4 tom = new Person4("Tom");
 //        System.out.println(tom.toString()); // Будет выводить что-то наподобие Person@7960847b
 //        System.out.println(tom.toString()); // Person Tom
-        System.out.println(tom.hashCode()); // 2036368507
+//        System.out.println(tom.hashCode()); // 2036368507
+        System.out.println(tom.getClass()); // class Person
     }
 }
 class Person4 {
@@ -19,9 +20,17 @@ class Person4 {
 
     @Override
 //    public String toString(){
-//
 //        return "Person " + name;
-    public int hashCode(){
-        return 10 * name.hashCode() + 20456;
+
+//    public int hashCode(){
+//        return 10 * name.hashCode() + 20456;
+
+
+    public boolean equals(Object obj){
+
+        if (!(obj instanceof Person)) return false;
+
+        Person p = (Person)obj;
+        return this.name.equals(p.name);
     }
 }
