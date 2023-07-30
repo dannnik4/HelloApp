f9ae67c77b6ebcce5cbfcf81a3fa6cad15a9377b
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-30 18:00:42 +0300

    Updated Generics.java

--- src/Chapter3/Generics.java
+++ src/Chapter3/Generics.java
@@ -3,6 +3,12 @@ package Chapter3;
 public class Generics {
     public static void main(String[] args) {
 
+        Account acc1 = new Account(2334, 5000); // id - число
+        int acc1Id = (int)acc1.getId();
+        System.out.println(acc1Id);
+
+        Account acc2 = new Account("sid5523", 5000);    // id - строка
+        System.out.println(acc2.getId());
     }
 }
 
