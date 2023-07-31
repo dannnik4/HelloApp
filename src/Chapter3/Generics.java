869348a7e0ef9b0c708c6b3aaa03d52235d3a9db
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-31 23:14:42 +0300

    Updated Generics.java

--- src/Chapter3/Generics.java
+++ src/Chapter3/Generics.java
@@ -50,8 +50,35 @@ public class Generics {
 //    public void setSum(int sum) { this.sum = sum; }
 //}
 
-        Accountable<String> acc1 = new Account("1235rwr", 5000);
-        Account acc2 = new Account("2373", 4300);
+
+//        Accountable<String> acc1 = new Account("1235rwr", 5000);
+//        Account acc2 = new Account("2373", 4300);
+//        System.out.println(acc1.getId());
+//        System.out.println(acc2.getId());
+//    }
+//}
+//interface Accountable<T>{
+//    T getId();
+//    int getSum();
+//    void setSum(int sum);
+//}
+//class Account implements Accountable<String>{
+//
+//    private String id;
+//    private int sum;
+//
+//    Account(String id, int sum){
+//        this.id = id;
+//        this.sum = sum;
+//    }
+//
+//    public String getId() { return id; }
+//    public int getSum() { return sum; }
+//    public void setSum(int sum) { this.sum = sum; }
+//}
+
+        Account<String> acc1 = new Account<String>("1235rwr", 5000);
+        Account<String> acc2 = new Account<String>("2373", 4300);
         System.out.println(acc1.getId());
         System.out.println(acc2.getId());
     }
@@ -61,17 +88,17 @@ interface Accountable<T>{
     int getSum();
     void setSum(int sum);
 }
-class Account implements Accountable<String>{
+class Account<T> implements Accountable<T>{
 
-    private String id;
+    private T id;
     private int sum;
 
-    Account(String id, int sum){
+    Account(T id, int sum){
         this.id = id;
         this.sum = sum;
     }
 
-    public String getId() { return id; }
+    public T getId() { return id; }
     public int getSum() { return sum; }
     public void setSum(int sum) { this.sum = sum; }
 }
