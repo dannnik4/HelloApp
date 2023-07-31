767452c8dd0ecf6957c2e97e46f23b213d21fa9d
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-31 23:15:08 +0300

    Updated Generics.java

--- src/Chapter3/Generics.java
+++ src/Chapter3/Generics.java
@@ -77,28 +77,45 @@ public class Generics {
 //    public void setSum(int sum) { this.sum = sum; }
 //}
 
-        Account<String> acc1 = new Account<String>("1235rwr", 5000);
-        Account<String> acc2 = new Account<String>("2373", 4300);
-        System.out.println(acc1.getId());
-        System.out.println(acc2.getId());
+//        Account<String> acc1 = new Account<String>("1235rwr", 5000);
+//        Account<String> acc2 = new Account<String>("2373", 4300);
+//        System.out.println(acc1.getId());
+//        System.out.println(acc2.getId());
+//    }
+//}
+//interface Accountable<T>{
+//    T getId();
+//    int getSum();
+//    void setSum(int sum);
+//}
+//class Account<T> implements Accountable<T>{
+//
+//    private T id;
+//    private int sum;
+//
+//    Account(T id, int sum){
+//        this.id = id;
+//        this.sum = sum;
+//    }
+//
+//    public T getId() { return id; }
+//    public int getSum() { return sum; }
+//    public void setSum(int sum) { this.sum = sum; }
+//}
+
+        Printer printer = new Printer();
+        String[] people = {"Tom", "Alice", "Sam", "Kate", "Bob", "Helen"};
+        Integer[] numbers = {23, 4, 5, 2, 13, 456, 4};
+        printer.<String>print(people);
+        printer.<Integer>print(numbers);
     }
 }
-interface Accountable<T>{
-    T getId();
-    int getSum();
-    void setSum(int sum);
-}
-class Account<T> implements Accountable<T>{
 
-    private T id;
-    private int sum;
+class Printer {
 
-    Account(T id, int sum){
-        this.id = id;
-        this.sum = sum;
+    public <T> void print(T[] items) {
+        for (T item : items) {
+            System.out.println(item);
+        }
     }
-
-    public T getId() { return id; }
-    public int getSum() { return sum; }
-    public void setSum(int sum) { this.sum = sum; }
 }
