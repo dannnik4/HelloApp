92ef79c82bae61bb05a7af48addcc821b6f84078
dannnik4 <daniilperstnev2001@gmail.com>
2023-07-31 23:15:56 +0300

    Updated Generics.java

--- src/Chapter3/Generics.java
+++ src/Chapter3/Generics.java
@@ -120,27 +120,49 @@ public class Generics {
 //    }
 //}
 
-        Account<String, Double> acc1 = new Account<String, Double>("354", 5000.87);
-        String id = acc1.getId();
-        Double sum = acc1.getSum();
-        System.out.printf("Id: %s  Sum: %f \n", id, sum);
+//        Account<String, Double> acc1 = new Account<String, Double>("354", 5000.87);
+//        String id = acc1.getId();
+//        Double sum = acc1.getSum();
+//        System.out.printf("Id: %s  Sum: %f \n", id, sum);
+//    }
+//}
+//class Account<T, S> {
+//
+//    private T id;
+//    private S sum;
+//
+//    Account(T id, S sum) {
+//        this.id = id;
+//        this.sum = sum;
+//    }
+//
+//    public T getId() {
+//        return id;
+//    }
+//
+//    public S getSum() {
+//        return sum;
+//    }
+//}
+
+        Account acc1 = new Account("cid2373", 5000);
+        Account acc2 = new Account(53757, 4000);
+        System.out.println(acc1.getId());
+        System.out.println(acc2.getId());
     }
 }
-class Account<T, S> {
 
-    private T id;
-    private S sum;
+class Account{
 
-    Account(T id, S sum) {
-        this.id = id;
+    private String id;
+    private int sum;
+
+    <T>Account(T id, int sum){
+        this.id = id.toString();
         this.sum = sum;
     }
 
-    public T getId() {
-        return id;
-    }
-
-    public S getSum() {
-        return sum;
-    }
+    public String getId() { return id; }
+    public int getSum() { return sum; }
+    public void setSum(int sum) { this.sum = sum; }
 }
