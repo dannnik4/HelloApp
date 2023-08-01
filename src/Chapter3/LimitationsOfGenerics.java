94de857933fa04a616609d54869fd919ef72c74f
dannnik4 <daniilperstnev2001@gmail.com>
2023-08-01 13:13:09 +0300

    Updated LimitationsOfGenerics.java

--- src/Chapter3/LimitationsOfGenerics.java
+++ src/Chapter3/LimitationsOfGenerics.java
@@ -52,16 +52,81 @@ public class LimitationsOfGenerics {
 //    public void setSum(int sum) { this.sum = sum; }
 //}
 
-        Account1<String> acc1 = new Account1<String>("1876", 4500);
-        Account1<String> acc2 = new Account1<String>("3476", 1500);
+//        Account1<String> acc1 = new Account1<String>("1876", 4500);
+//        Account1<String> acc2 = new Account1<String>("3476", 1500);
+//
+//        Transaction<Account1<String>> tran1 = new Transaction<Account1<String>>(acc1,acc2, 4000);
+//        tran1.execute();
+//        tran1 = new Transaction<Account1<String>>(acc1,acc2, 4000);
+//        tran1.execute();
+//    }
+//}
+//class Transaction<T extends Account1<String>>{
+//
+//    private T from;     // с какого счета перевод
+//    private T to;       // на какой счет перевод
+//    private int sum;    // сумма перевода
+//
+//    Transaction(T from, T to, int sum){
+//        this.from = from;
+//        this.to = to;
+//        this.sum = sum;
+//    }
+//    public void execute(){
+//
+//        if (from.getSum() > sum)
+//        {
+//            from.setSum(from.getSum() - sum);
+//            to.setSum(to.getSum() + sum);
+//            System.out.printf("Account %s: %d \nAccount %s: %d \n",
+//                    from.getId(), from.getSum(),to.getId(), to.getSum());
+//        }
+//        else{
+//            System.out.printf("Operation is invalid");
+//        }
+//    }
+//}
+//class Account1<T>{
+//
+//    private T id;
+//    private int sum;
+//
+//    Account1(T id, int sum){
+//        this.id = id;
+//        this.sum = sum;
+//    }
+//
+//    public T getId() { return id; }
+//    public int getSum() { return sum; }
+//    public void setSum(int sum) { this.sum = sum; }
+//}
 
-        Transaction<Account1<String>> tran1 = new Transaction<Account1<String>>(acc1,acc2, 4000);
-        tran1.execute();
-        tran1 = new Transaction<Account1<String>>(acc1,acc2, 4000);
+        Account1 acc1 = new Account1("1235rwr", 5000);
+        Account1 acc2 = new Account1("2373", 4300);
+        Transaction<Account1> tran1 = new Transaction<Account1>(acc1, acc2, 1560);
         tran1.execute();
     }
 }
-class Transaction<T extends Account1<String>>{
+interface Accountable{
+    String getId();
+    int getSum();
+    void setSum(int sum);
+}
+class Account1 implements Accountable{
+
+    private String id;
+    private int sum;
+
+    Account1(String id, int sum){
+        this.id = id;
+        this.sum = sum;
+    }
+
+    public String getId() { return id; }
+    public int getSum() { return sum; }
+    public void setSum(int sum) { this.sum = sum; }
+}
+class Transaction<T extends Accountable>{
 
     private T from;     // с какого счета перевод
     private T to;       // на какой счет перевод
@@ -85,18 +150,4 @@ class Transaction<T extends Account1<String>>{
             System.out.printf("Operation is invalid");
         }
     }
-}
-class Account1<T>{
-
-    private T id;
-    private int sum;
-
-    Account1(T id, int sum){
-        this.id = id;
-        this.sum = sum;
-    }
-
-    public T getId() { return id; }
-    public int getSum() { return sum; }
-    public void setSum(int sum) { this.sum = sum; }
 }
