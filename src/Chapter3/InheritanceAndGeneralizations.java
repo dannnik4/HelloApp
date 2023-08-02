2797ab20d9127973f846f61afe480db8bbf1da87
dannnik4 <daniilperstnev2001@gmail.com>
2023-08-02 23:37:58 +0300

    Updated InheritanceAndGeneralizations.java

--- src/Chapter3/InheritanceAndGeneralizations.java
+++ src/Chapter3/InheritanceAndGeneralizations.java
@@ -5,6 +5,23 @@ public class InheritanceAndGeneralizations {
 
     }
 
+//    class Account<T>
+//    {
+//        private T _id;
+//        T getId(){return _id;}
+//        Account(T id)
+//        {
+//            _id = id;
+//        }
+//    }
+//
+//    class DepositAccount<T> extends Account<T>{
+//
+//        DepositAccount(T id){
+//            super(id);
+//        }
+//    }
+
     class Account<T>
     {
         private T _id;
@@ -15,10 +32,13 @@ public class InheritanceAndGeneralizations {
         }
     }
 
-    class DepositAccount<T> extends Account<T>{
+    class DepositAccount<T, S> extends Account<T>{
 
-        DepositAccount(T id){
+        private S _name;
+        S getName(){return _name;}
+        DepositAccount(T id, S name){
             super(id);
+            this._name=name;
         }
     }
 }
