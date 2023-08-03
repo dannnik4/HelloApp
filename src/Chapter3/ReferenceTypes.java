66c6a3a43f7fce24c36c7928cb863c1b6d1cf564
dannnik4 <daniilperstnev2001@gmail.com>
2023-08-03 19:17:23 +0300

    Added ReferenceTypes.java

---
+++ src/Chapter3/ReferenceTypes.java
@@ -0,0 +1,30 @@
+package Chapter3;
+
+public class ReferenceTypes {
+    public static void main(String[] args) {
+
+        Person5 tom = new Person5("Tom", 23);
+        tom.display();      // Person Tom
+        Person5 bob = tom;
+        bob.setName("Bob");
+        tom.display();      // Person Bob
+    }
+}
+class Person5{
+    private String name;
+    private int age;
+
+    Person5(String name, int age){
+        this.name=name;
+        this.age=age;
+    }
+    void setName(String name){
+        this.name = name;
+    }
+    void setAge(int age){
+        this.age = age;
+    }
+    void display(){
+        System.out.printf("Person Name: %s \n", name);
+    }
+}
