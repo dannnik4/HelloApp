1b90565ac39ba5bd30888b88cd0b4b6c4d4707df
dannnik4 <daniilperstnev2001@gmail.com>
2023-08-04 13:09:39 +0300

    Updated Records.java

--- src/Chapter3/Records.java
+++ src/Chapter3/Records.java
@@ -5,8 +5,19 @@ import java.util.Objects;
 public class Records {
     public static void main(String[] args) {
 
-        Person tom = new Person("Tom", 36);
-        System.out.println(tom.toString());
+//        Person tom = new Person("Tom", 36);
+//        System.out.println(tom.toString());
+
+        Person6 tom = new Person6("Tom",  36);
+
+        System.out.println(tom.name());     // Tom
+        System.out.println(tom.age());      // 36
+        System.out.println(tom.hashCode());
+
+        Person bob = new Person("Bob", 21);
+        Person tomas = new Person("Tom", 36);
+        System.out.println(tom.equals(bob));    // false
+        System.out.println(tom.equals(tomas));  // true
     }
 }
 
