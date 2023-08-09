f3778e68a09d23fe7999f3bb6cc97e25b1613d12
dannnik4 <daniilperstnev2001@gmail.com>
2023-08-09 19:33:09 +0300

    Updated ExceptionHandling.java

--- src/Chapter4/ExceptionHandling.java
+++ src/Chapter4/ExceptionHandling.java
@@ -14,13 +14,32 @@ public class ExceptionHandling {
         }
     }
 
-    public static int getFactorial(int num) throws Exception {
+//    public static int getFactorial(int num) throws Exception {
+//
+//        if (num < 1) throw new Exception("The number is less than 1");
+//        int result = 1;
+//        for (int i = 1; i <= num; i++) {
+//
+//            result *= i;
+//        }
+//        return result;
+//    }
 
-        if (num < 1) throw new Exception("The number is less than 1");
-        int result = 1;
-        for (int i = 1; i <= num; i++) {
+    public static int getFactorial(int num){
 
-            result *= i;
+        int result=1;
+        try{
+            if(num<1) throw new Exception("The number is less than 1");
+
+            for(int i=1; i<=num;i++){
+
+                result*=i;
+            }
+        }
+        catch(Exception ex){
+
+            System.out.println(ex.getMessage());
+            result=num;
         }
         return result;
     }
