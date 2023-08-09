cead24a59828de21f387b6ad063e09df4a88dbef
dannnik4 <daniilperstnev2001@gmail.com>
2023-08-09 19:32:42 +0300

    Updated ExceptionHandling.java

--- src/Chapter4/ExceptionHandling.java
+++ src/Chapter4/ExceptionHandling.java
@@ -1,8 +1,17 @@
 package Chapter4;
 
 public class ExceptionHandling {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
+        try{
+            int result = getFactorial(-6);
+
+            System.out.println(result);
+        }
+        catch(Exception ex){
+
+            System.out.println(ex.getMessage());
+        }
     }
 
     public static int getFactorial(int num) throws Exception {
