ae043a3c0f02d46306ae8d6213d0adf5cac51348
dannnik4 <daniilperstnev2001@gmail.com>
2023-08-10 10:51:05 +0300

    Updated ExceptionClasses.java

--- src/Chapter4/ExceptionClasses.java
+++ src/Chapter4/ExceptionClasses.java
@@ -1,12 +1,18 @@
 package Chapter4;
 
 public class ExceptionClasses {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        try {
+            int x = 6 / 0;
+        } catch (Exception ex) {
 
+            ex.printStackTrace();
+        }
     }
-    public Person clone() throws CloneNotSupportedException{
 
-        Person p = (Person) super.clone();
-        return p;
-    }
+//    public Person clone() throws CloneNotSupportedException {
+//
+//        Person p = (Person) super.clone();
+//        return p;
+//    }
 }
