f5584b61cc768dad9e16158f6f3e56bb28e3e992
dannnik4 <daniilperstnev2001@gmail.com>
2023-08-11 13:16:09 +0300

    Added OwnExceptionClasses.java

---
+++ src/Chapter4/OwnExceptionClasses.java
@@ -0,0 +1,30 @@
+package Chapter4;
+
+public class OwnExceptionClasses {
+    public static void main(String[] args) {
+
+    }
+
+    public static int getFactorial(int num) throws FactorialException{
+
+        int result=1;
+        if(num<1) throw new FactorialException("The number is less than 1", num);
+
+        for(int i=1; i<=num;i++){
+
+            result*=i;
+        }
+        return result;
+    }
+}
+
+class FactorialException extends Exception{
+
+    private int number;
+    public int getNumber(){return number;}
+    public FactorialException(String message, int num){
+
+        super(message);
+        number=num;
+    }
+}
