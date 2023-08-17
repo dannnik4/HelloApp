ee9dea6254fe44b2df4197c26a1263edb6145ffd
dannnik4 <daniilperstnev2001@gmail.com>
2023-08-17 23:49:12 +0300

    Added SortedSetNavigableSetTreeSet.java

---
+++ src/Chapter5/SortedSetNavigableSetTreeSet.java
@@ -0,0 +1,26 @@
+package Chapter5;
+
+import java.util.TreeSet;
+
+public class SortedSetNavigableSetTreeSet {
+    public static void main(String[] args) {
+
+        TreeSet<String> states = new TreeSet<String>();
+
+        // добавим в список ряд элементов
+        states.add("Germany");
+        states.add("France");
+        states.add("Italy");
+        states.add("Great Britain");
+
+        System.out.printf("TreeSet contains %d elements \n", states.size());
+
+        // удаление элемента
+        states.remove("Germany");
+        for(String state : states){
+
+            System.out.println(state);
+        }
+    }
+}
+}
