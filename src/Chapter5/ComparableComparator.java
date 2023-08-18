df8f6eae7b8fb20481c2697ee763a9d42e8b8d1c
dannnik4 <daniilperstnev2001@gmail.com>
2023-08-18 23:52:21 +0300

    Added ComparableComparator.java

---
+++ src/Chapter5/ComparableComparator.java
@@ -0,0 +1,21 @@
+package Chapter5;
+
+public class ComparableComparator {
+    public static void main(String[] args) {
+
+    }
+
+    class Person3 {
+
+        private String name;
+
+        Person3(String name) {
+
+            this.name = name;
+        }
+
+        String getName() {
+            return name;
+        }
+    }
+}
