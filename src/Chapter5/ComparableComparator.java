373ea3cbe7bdd7c595ec27a9c6f25a6dc3571f37
dannnik4 <daniilperstnev2001@gmail.com>
2023-08-20 23:18:41 +0300

    Updated ComparableComparator.java

--- src/Chapter5/ComparableComparator.java
+++ src/Chapter5/ComparableComparator.java
@@ -4,18 +4,18 @@ import java.util.TreeSet;
 
 public class ComparableComparator {
     public static void main(String[] args) {
-        TreeSet<Person3> people = new TreeSet<Person3>();
-        people.add(new Person3("Tom"));
+//        TreeSet<Person3> people = new TreeSet<Person3>();
+//        people.add(new Person3("Tom"));
 
 //        PersonComparator pcomp = new PersonComparator();
 //        TreeSet<Person3> people = new TreeSet<Person3>(pcomp);
-        people.add(new Person3("Tom"));
-        people.add(new Person3("Nick"));
-        people.add(new Person3("Alice"));
-        people.add(new Person3("Bill"));
-        for(Person3  p : people){
-
-            System.out.println(p.getName());
+//        people.add(new Person3("Tom"));
+//        people.add(new Person3("Nick"));
+//        people.add(new Person3("Alice"));
+//        people.add(new Person3("Bill"));
+//        for(Person3  p : people){
+//
+//            System.out.println(p.getName());
         }
     }
 
@@ -33,37 +33,50 @@ public class ComparableComparator {
 //        }
 //    }
 
-    static class Person3 implements Comparable<Person3>{
-
-        private String name;
-        Person3(String name){
-
-            this.name = name;
-        }
-        String getName(){return name;}
-
+//    static class Person3 implements Comparable<Person3>{
+//
+//        private String name;
+//        Person3(String name){
+//
+//            this.name = name;
+//        }
+//        String getName(){return name;}
+//
+////        public int compareTo(Person3 p){
+////
+////            return name.compareTo(p.getName());
+////        }
+//
 //        public int compareTo(Person3 p){
 //
-//            return name.compareTo(p.getName());
+//            return name.length()-p.getName().length();
 //        }
+//    }
+}
 
-        public int compareTo(Person3 p){
+    class Person3{
 
-            return name.length()-p.getName().length();
+        private String name;
+        private int age;
+        public Person3(String n, int a){
+
+            name=n;
+            age=a;
         }
+        String getName(){return name;}
+        int getAge(){return age;}
     }
 
-    static abstract class PersonComparator implements Comparator<Person3>{
+//    static abstract class PersonComparator implements Comparator<Person3>{
+//
+//        public int compare(Person a, Person b){
+//
+//            return a.getName().compareTo(b.getName());
+//        }
+//    }
 
-        public int compare(Person a, Person b){
-
-            return a.getName().compareTo(b.getName());
-        }
-    }
-
-    public interface Comparator<E> {
+//    public interface Comparator<E> {
 
 //        int compare(T a, T b);
-        // остальные методы
-    }
-}
+//        // остальные методы
+//    }
