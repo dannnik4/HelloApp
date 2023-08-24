e5d0ddf50348f6d486cdbce09126537908d941d7
dannnik4 <daniilperstnev2001@gmail.com>
2023-08-24 23:41:34 +0300

    Updated Iterators.java

--- src/Chapter5/Iterators.java
+++ src/Chapter5/Iterators.java
@@ -1,10 +1,30 @@
 package Chapter5;
 
+import java.util.ArrayList;
+
 public class Iterators {
-    public interface Iterator <E>{
+
+    public static void main(String[] args) {
+
+        ArrayList<String> states = new ArrayList<String>();
+        states.add("Germany");
+        states.add("France");
+        states.add("Italy");
+        states.add("Spain");
+
+        Iterator<String> iter = (Iterator<String>) states.iterator();
+        while (iter.hasNext()) {
+
+            System.out.println(iter.next());
+        }
+    }
+
+    public interface Iterator<E> {
 
         E next();
+
         boolean hasNext();
+
         void remove();
     }
 }
