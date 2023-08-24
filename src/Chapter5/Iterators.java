bf48ba4c0f8831cafb05cbc5e326bc30511d14d7
dannnik4 <daniilperstnev2001@gmail.com>
2023-08-24 23:42:00 +0300

    Updated Iterators.java

--- src/Chapter5/Iterators.java
+++ src/Chapter5/Iterators.java
@@ -1,10 +1,24 @@
 package Chapter5;
 
 import java.util.ArrayList;
+import java.util.ListIterator;
 
 public class Iterators {
 
     public static void main(String[] args) {
+
+//        ArrayList<String> states = new ArrayList<String>();
+//        states.add("Germany");
+//        states.add("France");
+//        states.add("Italy");
+//        states.add("Spain");
+//
+//        Iterator<String> iter = (Iterator<String>) states.iterator();
+//        while (iter.hasNext()) {
+//
+//            System.out.println(iter.next());
+//        }
+//    }
 
         ArrayList<String> states = new ArrayList<String>();
         states.add("Germany");
@@ -12,10 +26,19 @@ public class Iterators {
         states.add("Italy");
         states.add("Spain");
 
-        Iterator<String> iter = (Iterator<String>) states.iterator();
-        while (iter.hasNext()) {
+        ListIterator<String> listIter = states.listIterator();
 
-            System.out.println(iter.next());
+        while(listIter.hasNext()){
+
+            System.out.println(listIter.next());
+        }
+        // сейчас текущий элемент - Испания
+        // изменим значение этого элемента
+        listIter.set("Португалия");
+        // пройдемся по элементам в обратном порядке
+        while(listIter.hasPrevious()){
+
+            System.out.println(listIter.previous());
         }
     }
 
