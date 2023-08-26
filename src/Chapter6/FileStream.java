969fd4b60f3f0f3386f58ab21de5b0503a00ef7d
dannnik4 <daniilperstnev2001@gmail.com>
2023-08-26 13:22:56 +0300

    Updated FileStream.java

--- src/Chapter6/FileStream.java
+++ src/Chapter6/FileStream.java
@@ -1,20 +1,34 @@
 package Chapter6;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class FileStream {
     public static void main(String[] args) {
-        String text = "Hello world!"; // строка для записи
-        try (
-                FileOutputStream fos = new FileOutputStream("notes.txt")) {
-            // перевод строки в байты
-            byte[] buffer = text.getBytes();
+//        String text = "Hello world!"; // строка для записи
+//        try (
+//                FileOutputStream fos = new FileOutputStream("notes.txt")) {
+//            // перевод строки в байты
+//            byte[] buffer = text.getBytes();
+//
+//            fos.write(buffer, 0, buffer.length);
+//            System.out.println("The file has been written");
+//        } catch (
+//                IOException ex) {
+//
+//            System.out.println(ex.getMessage());
+//        }
 
-            fos.write(buffer, 0, buffer.length);
-            System.out.println("The file has been written");
-        } catch (
-                IOException ex) {
+        try(FileInputStream fin=new FileInputStream("notes.txt"))
+        {
+            int i;
+            while((i=fin.read())!=-1){
+
+                System.out.print((char)i);
+            }
+        }
+        catch(IOException ex){
 
             System.out.println(ex.getMessage());
         }
