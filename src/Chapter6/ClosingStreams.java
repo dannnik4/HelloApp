2a126e4dd0f55f055df724f0608dfa3b3162062e
dannnik4 <daniilperstnev2001@gmail.com>
2023-08-27 12:15:44 +0300

    Added ClosingStreams.java

---
+++ src/Chapter6/ClosingStreams.java
@@ -0,0 +1,37 @@
+package Chapter6;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ClosingStreams {
+    public static void main(String[] args) {
+
+        FileInputStream fin=null;
+        try
+        {
+            fin = new FileInputStream("C://SomeDir//notes.txt");
+
+            int i=-1;
+            while((i=fin.read())!=-1){
+
+                System.out.print((char)i);
+            }
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+        finally{
+
+            try{
+
+                if(fin!=null)
+                    fin.close();
+            }
+            catch(IOException ex){
+
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
