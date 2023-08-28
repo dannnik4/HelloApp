dfb78ad42c3e33e01385d2dfc89d01ff4935e99f
dannnik4 <daniilperstnev2001@gmail.com>
2023-08-28 22:13:20 +0300

    Added ByteArrayInputOutputStream.java

--- src/Chapter6/ByteArrayInputOutputStream.java
+++ src/Chapter6/ByteArrayInputOutputStream.java
@@ -1,26 +1,48 @@
 package Chapter6;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 
 public class ByteArrayInputOutputStream {
     public static void main(String[] args) {
 
-        byte[] array1 = new byte[]{1, 3, 5, 7};
-        ByteArrayInputStream byteStream1 = new ByteArrayInputStream(array1);
-        int b;
-        while((b=byteStream1.read())!=-1){
+//        byte[] array1 = new byte[]{1, 3, 5, 7};
+//        ByteArrayInputStream byteStream1 = new ByteArrayInputStream(array1);
+//        int b;
+//        while((b=byteStream1.read())!=-1){
+//
+//            System.out.println(b);
+//        }
+//
+//        String text = "Hello world!";
+//        byte[] array2 = text.getBytes();
+//        // считываем 5 символов
+//        ByteArrayInputStream byteStream2 = new ByteArrayInputStream(array2, 0, 5);
+//        int c;
+//        while((c=byteStream2.read())!=-1){
+//
+//            System.out.println((char)c);
+//        }
 
-            System.out.println(b);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        String text = "Hello Wolrd!";
+        byte[] buffer = text.getBytes();
+        try{
+            baos.write(buffer);
         }
+        catch(Exception ex){
 
-        String text = "Hello world!";
-        byte[] array2 = text.getBytes();
-        // считываем 5 символов
-        ByteArrayInputStream byteStream2 = new ByteArrayInputStream(array2, 0, 5);
-        int c;
-        while((c=byteStream2.read())!=-1){
-
-            System.out.println((char)c);
+            System.out.println(ex.getMessage());
         }
+        // превращаем массив байтов в строку
+        System.out.println(baos.toString());
+
+        // получаем массив байтов и выводим по символьно
+        byte[] array = baos.toByteArray();
+        for(byte b: array){
+
+            System.out.print((char)b);
+        }
+        System.out.println();
     }
 }
