3a31ae465e90c2c7bfadec808d29ef7421ef6792
dannnik4 <daniilperstnev2001@gmail.com>
2023-08-29 22:32:12 +0300

    Updated BufferedInputOutputStream.java

--- src/Chapter6/BufferedInputOutputStream.java
+++ src/Chapter6/BufferedInputOutputStream.java
@@ -1,27 +1,39 @@
 package Chapter6;
 
-import java.io.BufferedInputStream;
-import java.io.ByteArrayInputStream;
+import java.io.*;
 
 public class BufferedInputOutputStream {
     public static void main(String[] args) {
 
-        String text = "Hello world!";
-        byte[] buffer = text.getBytes();
-        ByteArrayInputStream in = new ByteArrayInputStream(buffer);
+//        String text = "Hello world!";
+//        byte[] buffer = text.getBytes();
+//        ByteArrayInputStream in = new ByteArrayInputStream(buffer);
+//
+//        try(BufferedInputStream bis = new BufferedInputStream(in)){
+//
+//            int c;
+//            while((c=bis.read())!=-1){
+//
+//                System.out.print((char)c);
+//            }
+//        }
+//        catch(Exception e){
+//
+//            System.out.println(e.getMessage());
+//        }
+//        System.out.println();
 
-        try(BufferedInputStream bis = new BufferedInputStream(in)){
-
-            int c;
-            while((c=bis.read())!=-1){
-
-                System.out.print((char)c);
-            }
+        String text = "Hello world!"; // строка для записи
+        try(FileOutputStream out=new FileOutputStream("notes.txt");
+            BufferedOutputStream bos = new BufferedOutputStream(out))
+        {
+            // перевод строки в байты
+            byte[] buffer = text.getBytes();
+            bos.write(buffer, 0, buffer.length);
         }
-        catch(Exception e){
+        catch(IOException ex){
 
-            System.out.println(e.getMessage());
+            System.out.println(ex.getMessage());
         }
-        System.out.println();
     }
 }
