eed58c4402ebc674eec443f34f73a3c78889919f
dannnik4 <daniilperstnev2001@gmail.com>
2023-09-01 10:58:30 +0300

    Updated ReadWriteTXT.java

--- src/Chapter6/ReadWriteTXT.java
+++ src/Chapter6/ReadWriteTXT.java
@@ -1,21 +1,36 @@
 package Chapter6;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class ReadWriteTXT {
     public static void main(String[] args) {
 
-        try(FileWriter writer = new FileWriter("notes3.txt", false))
-        {
-            // запись всей строки
-            String text = "Hello Gold!";
-            writer.write(text);
-            // запись по символам
-            writer.append('\n');
-            writer.append('E');
+//        try(FileWriter writer = new FileWriter("notes3.txt", false))
+//        {
+//            // запись всей строки
+//            String text = "Hello Gold!";
+//            writer.write(text);
+//            // запись по символам
+//            writer.append('\n');
+//            writer.append('E');
+//
+//            writer.flush();
+//        }
+//        catch(IOException ex){
+//
+//            System.out.println(ex.getMessage());
+//        }
 
-            writer.flush();
+        try(FileReader reader = new FileReader("notes3.txt"))
+        {
+            // читаем посимвольно
+            int c;
+            while((c=reader.read())!=-1){
+
+                System.out.print((char)c);
+            }
         }
         catch(IOException ex){
 
