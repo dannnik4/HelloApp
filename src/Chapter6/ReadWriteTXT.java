31dc839708dd0bfcd3e09428db1b4eca42814f74
dannnik4 <daniilperstnev2001@gmail.com>
2023-09-01 10:57:55 +0300

    Added ReadWriteTXT.java

---
+++ src/Chapter6/ReadWriteTXT.java
@@ -0,0 +1,25 @@
+package Chapter6;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ReadWriteTXT {
+    public static void main(String[] args) {
+
+        try(FileWriter writer = new FileWriter("notes3.txt", false))
+        {
+            // запись всей строки
+            String text = "Hello Gold!";
+            writer.write(text);
+            // запись по символам
+            writer.append('\n');
+            writer.append('E');
+
+            writer.flush();
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+    }
+}
