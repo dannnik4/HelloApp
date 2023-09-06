e7f6d178012ab734b3dbd15f085a8c4a8f7a66fd
dannnik4 <daniilperstnev2001@gmail.com>
2023-09-06 22:39:18 +0300

    Updated ClassFile.java

--- src/Chapter6/ClassFile.java
+++ src/Chapter6/ClassFile.java
@@ -4,21 +4,34 @@ import java.io.File;
 
 public class ClassFile {
     public static void main(String[] args) {
+//        // определяем объект для каталога
+//        File dir = new File("C://SomeDir");
+//        // если объект представляет каталог
+//        if (dir.isDirectory()) {
+//            // получаем все вложенные объекты в каталоге
+//            for (File item : dir.listFiles()) {
+//
+//                if (item.isDirectory()) {
+//
+//                    System.out.println(item.getName() + "  \t folder");
+//                } else {
+//
+//                    System.out.println(item.getName() + "\t file");
+//                }
+//            }
+//        }
+
         // определяем объект для каталога
-        File dir = new File("C://SomeDir");
-        // если объект представляет каталог
-        if (dir.isDirectory()) {
-            // получаем все вложенные объекты в каталоге
-            for (File item : dir.listFiles()) {
-
-                if (item.isDirectory()) {
-
-                    System.out.println(item.getName() + "  \t folder");
-                } else {
-
-                    System.out.println(item.getName() + "\t file");
-                }
-            }
-        }
+        File dir = new File("C://SomeDir//NewDir");
+        boolean created = dir.mkdir();
+        if(created)
+            System.out.println("Folder has been created");
+        // переименуем каталог
+        File newDir = new File("C://SomeDir//NewDirRenamed");
+        dir.renameTo(newDir);
+        // удалим каталог
+        boolean deleted = newDir.delete();
+        if(deleted)
+            System.out.println("Folder has been deleted");
     }
 }
