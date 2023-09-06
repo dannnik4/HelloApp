456f199a4d4c077f839b0017ea741d55ab84ed83
dannnik4 <daniilperstnev2001@gmail.com>
2023-09-06 22:39:55 +0300

    Updated ClassFile.java

--- src/Chapter6/ClassFile.java
+++ src/Chapter6/ClassFile.java
@@ -1,6 +1,7 @@
 package Chapter6;
 
 import java.io.File;
+import java.io.IOException;
 
 public class ClassFile {
     public static void main(String[] args) {
@@ -21,17 +22,50 @@ public class ClassFile {
 //            }
 //        }
 
+//        // определяем объект для каталога
+//        File dir = new File("C://SomeDir//NewDir");
+//        boolean created = dir.mkdir();
+//        if(created)
+//            System.out.println("Folder has been created");
+//        // переименуем каталог
+//        File newDir = new File("C://SomeDir//NewDirRenamed");
+//        dir.renameTo(newDir);
+//        // удалим каталог
+//        boolean deleted = newDir.delete();
+//        if(deleted)
+//            System.out.println("Folder has been deleted");
+
         // определяем объект для каталога
-        File dir = new File("C://SomeDir//NewDir");
-        boolean created = dir.mkdir();
-        if(created)
-            System.out.println("Folder has been created");
-        // переименуем каталог
-        File newDir = new File("C://SomeDir//NewDirRenamed");
-        dir.renameTo(newDir);
-        // удалим каталог
-        boolean deleted = newDir.delete();
-        if(deleted)
-            System.out.println("Folder has been deleted");
+        File myFile = new File("C://SomeDir//notes.txt");
+        System.out.println("File name: " + myFile.getName());
+        System.out.println("Parent folder: " + myFile.getParent());
+        if(myFile.exists())
+            System.out.println("File exists");
+        else
+            System.out.println("File not found");
+
+        System.out.println("File size: " + myFile.length());
+        if(myFile.canRead())
+            System.out.println("File can be read");
+        else
+            System.out.println("File can not be read");
+
+        if(myFile.canWrite())
+            System.out.println("File can be written");
+        else
+            System.out.println("File can not be written");
+
+        // создадим новый файл
+        File newFile = new File("C://SomeDir//MyFile");
+        try
+        {
+            boolean created = newFile.createNewFile();
+            if(created)
+                System.out.println("File has been created");
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
     }
 }
