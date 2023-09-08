af1cadba5bc92f29f815439114f72858fcfa67fc
dannnik4 <daniilperstnev2001@gmail.com>
2023-09-08 23:39:59 +0300

    Added ClassConsole.java

---
+++ src/Chapter6/ClassConsole.java
@@ -0,0 +1,19 @@
+package Chapter6;
+
+import java.io.Console;
+
+public class ClassConsole {
+    public static void main(String[] args) {
+
+        // получаем консоль
+        Console console = System.console();
+        if(console!=null){
+            // считываем данные с консоли
+            String login = console.readLine("Введите логин:");
+            char[] password = console.readPassword("Введите пароль:");
+
+            console.printf("Введенный логин: %s \n", login);
+            console.printf("Введенный пароль: %s \n", new String(password));
+        }
+    }
+}
