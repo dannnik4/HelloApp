a2452413dd6152f7bc962c89a59d2049afa619b1
dannnik4 <daniilperstnev2001@gmail.com>
2023-09-09 21:51:18 +0300

    Added ClassString.java

---
+++ src/Chapter7/ClassString.java
@@ -0,0 +1,16 @@
+package Chapter7;
+
+public class ClassString {
+    public static void main(String[] args) {
+
+        String str1 = "Java";
+        String str2 = new String(); // пустая строка
+        String str3 = new String(new char[] {'h', 'e', 'l', 'l', 'o'});
+        String str4 = new String(new char[]{'w', 'e', 'l', 'c', 'o', 'm', 'e'}, 3, 4);//3 -начальный индекс, 4 -кол-во символов
+
+        System.out.println(str1); // Java
+        System.out.println(str2); //
+        System.out.println(str3); // hello
+        System.out.println(str4); // come
+    }
+}
