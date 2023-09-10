c2251daa7cd5877ed553d82b97241a77075caeab
dannnik4 <daniilperstnev2001@gmail.com>
2023-09-10 13:57:30 +0300

    Added BasicStringOperations.java

---
+++ src/Chapter7/BasicStringOperations.java
@@ -0,0 +1,11 @@
+package Chapter7;
+
+public class BasicStringOperations {
+    public static void main(String[] args) {
+        String str1 = "Java";
+        String str2 = "Hello";
+        String str3 = str1 + " " + str2;
+
+        System.out.println(str3); // Hello Java
+    }
+}
