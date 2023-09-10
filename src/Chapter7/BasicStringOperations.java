c02a8279c96e93bdb9c63ec3ac41b5f6c7a03653
dannnik4 <daniilperstnev2001@gmail.com>
2023-09-10 13:58:17 +0300

    Updated BasicStringOperations.java

--- src/Chapter7/BasicStringOperations.java
+++ src/Chapter7/BasicStringOperations.java
@@ -4,8 +4,11 @@ public class BasicStringOperations {
     public static void main(String[] args) {
         String str1 = "Java";
         String str2 = "Hello";
-        String str3 = str1 + " " + str2;
+//        String str3 = str1 + " " + str2;
+//
+//        System.out.println(str3); // Hello Java
 
-        System.out.println(str3); // Hello Java
+        String str3 = "Год " + 2015;
+        System.out.println(str3);
     }
 }
