07cad5a562e6dad8b4250dd52555dd3dec449387
dannnik4 <daniilperstnev2001@gmail.com>
2023-09-12 20:59:29 +0300

    Updated BasicStringOperations.java

--- src/Chapter7/BasicStringOperations.java
+++ src/Chapter7/BasicStringOperations.java
@@ -25,7 +25,7 @@ public class BasicStringOperations {
         int start = 6;
         int end = 11;
         char[] dst=new char[end - start];
-        str.getChars(start, end, dst, 0);
+//        str.getChars(start, end, dst, 0);
         System.out.println(dst); // world
 
         System.out.println(str1.equals(str2)); // false
@@ -38,9 +38,9 @@ public class BasicStringOperations {
         System.out.println(str1.compareTo(str2)); // -15 - str1 меньше чем strt2
         System.out.println(str1.compareTo(str3)); // 1 - str1 больше чем str3
 
-        int index1 = str.indexOf('l'); // 2
-        int index2 = str.indexOf("wo"); //6
-        int index3 = str.lastIndexOf('l'); //9
+//        int index1 = str.indexOf('l'); // 2
+//        int index2 = str.indexOf("wo"); //6
+//        int index3 = str.lastIndexOf('l'); //9
 
 //        String str = "myfile.exe";
 //        boolean start = str.startsWith("my"); //true
