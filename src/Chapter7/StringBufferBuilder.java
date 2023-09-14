66e8fa9aa68f17bcd9f5d5b709548d73caf50baf
dannnik4 <daniilperstnev2001@gmail.com>
2023-09-14 22:39:18 +0300

    Added StringBufferBuilder.java

---
+++ src/Chapter7/StringBufferBuilder.java
@@ -0,0 +1,12 @@
+package Chapter7;
+
+public class StringBufferBuilder {
+    public static void main(String[] args) {
+        String str = "Java";
+        StringBuffer strBuffer = new StringBuffer(str);
+        System.out.println("Емкость: " + strBuffer.capacity()); // 20
+        strBuffer.ensureCapacity(32);
+        System.out.println("Емкость: " + strBuffer.capacity()); // 42
+        System.out.println("Длина: " + strBuffer.length()); // 4
+    }
+}
