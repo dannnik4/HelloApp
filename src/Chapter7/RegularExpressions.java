5cd88c0f413f1115f07059eeb6fb0852b8867a73
dannnik4 <daniilperstnev2001@gmail.com>
2023-09-19 22:31:06 +0300

    Updated RegularExpressions.java

--- src/Chapter7/RegularExpressions.java
+++ src/Chapter7/RegularExpressions.java
@@ -2,10 +2,19 @@ package Chapter7;
 
 public class RegularExpressions {
     public static void main(String[] args) {
-        String text = "FIFA will never regret it";
-        String[] words = text.split("\\s*(\\s|,|!|\\.)\\s*");
-        for(String word : words){
-            System.out.println(word);
+//        String text = "FIFA will never regret it";
+//        String[] words = text.split("\\s*(\\s|,|!|\\.)\\s*");
+//        for(String word : words){
+//            System.out.println(word);
+//        }
+
+        String input = "+12343454556";
+        boolean result = input.matches("(\\+*)\\d{11}");
+        if(result){
+            System.out.println("It is a phone number");
+        }
+        else{
+            System.out.println("It is not a phone number!");
         }
     }
 }
