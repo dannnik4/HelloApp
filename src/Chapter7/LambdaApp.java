c3a623d96fc55559060d18c6b3c8c61d15936648
dannnik4 <daniilperstnev2001@gmail.com>
2023-09-22 22:50:25 +0300

    Added LambdaApp.java

---
+++ src/Chapter7/LambdaApp.java
@@ -0,0 +1,15 @@
+package Chapter7;
+
+public class LambdaApp {
+    public static void main(String[] args) {
+
+        Operationable operation;
+        operation = (x,y)->x+y;
+
+        int result = operation.calculate(10, 20);
+        System.out.println(result); //30
+    }
+}
+interface Operationable{
+    int calculate(int x, int y);
+}
