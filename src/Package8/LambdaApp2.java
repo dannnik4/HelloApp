7e2153f0c67b69bd77dbb21ce79e7d507a1ae1f6
dannnik4 <daniilperstnev2001@gmail.com>
2023-09-27 08:19:23 +0300

    Updated LambdaApp2.java

--- src/Package8/LambdaApp2.java
+++ src/Package8/LambdaApp2.java
@@ -87,23 +87,45 @@ public class LambdaApp2 {
 //    }
 //}
 
-        UserBuilder userBuilder = User::new;
-        User user = userBuilder.create("Tom");
-        System.out.println(user.getName());
+//        UserBuilder userBuilder = User::new;
+//        User user = userBuilder.create("Tom");
+//        System.out.println(user.getName());
+//    }
+//}
+//interface UserBuilder{
+//    User create(String name);
+//}
+//
+//class User{
+//
+//    private String name;
+//    String getName(){
+//        return name;
+//    }
+//
+//    User(String n){
+//        this.name=n;
+//    }
+//}
+
+        Operation func = action(1);
+        int a = func.execute(6, 5);
+        System.out.println(a);          // 11
+
+        int b = action(2).execute(8, 2);
+        System.out.println(b);          // 6
+    }
+
+    private static Operation action(int number){
+        switch(number){
+            case 1: return (x, y) -> x + y;
+            case 2: return (x, y) -> x - y;
+            case 3: return (x, y) -> x * y;
+            default: return (x,y) -> 0;
+        }
     }
 }
-interface UserBuilder{
-    User create(String name);
-}
 
-class User{
-
-    private String name;
-    String getName(){
-        return name;
-    }
-
-    User(String n){
-        this.name=n;
-    }
+interface Operation{
+    int execute(int x, int y);
 }
