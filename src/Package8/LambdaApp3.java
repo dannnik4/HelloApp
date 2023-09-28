afa7167ca36b4d1f0664673c208656a9500a4a0f
dannnik4 <daniilperstnev2001@gmail.com>
2023-09-28 13:38:42 +0300

    Updated LambdaApp3.java

--- src/Package8/LambdaApp3.java
+++ src/Package8/LambdaApp3.java
@@ -3,13 +3,22 @@ package Package8;
 public class LambdaApp3 {
     public static void main(String[] args) {
 
-        Predicate<Integer> isPositive = x -> x > 0;
+//        Predicate<Integer> isPositive = x -> x > 0;
+//
+//        System.out.println(isPositive.test(5)); // true
+//        System.out.println(isPositive.test(-7)); // false
 
-        System.out.println(isPositive.test(5)); // true
-        System.out.println(isPositive.test(-7)); // false
+        BinaryOperator<Integer> multiply = (x, y) -> x * y;
+
+        System.out.println(multiply.apply(3, 5)); // 15
+        System.out.println(multiply.apply(10, -2)); // -20
     }
 }
 
-interface Predicate<T> {
-    boolean test(T t);
+//interface Predicate<T> {
+//    boolean test(T t);
+//}
+
+interface BinaryOperator<T> {
+    T apply(T t1, T t2);
 }
