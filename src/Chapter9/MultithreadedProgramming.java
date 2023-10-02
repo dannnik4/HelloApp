d3c04eea8f8f07b4a850518cf47b2a47b50d9d48
dannnik4 <daniilperstnev2001@gmail.com>
2023-10-02 13:21:36 +0300

    Updated MultithreadedProgramming.java

--- src/Chapter9/MultithreadedProgramming.java
+++ src/Chapter9/MultithreadedProgramming.java
@@ -3,7 +3,10 @@ package Chapter9;
 public class MultithreadedProgramming {
     public static void main(String[] args) {
 
+//        Thread t = Thread.currentThread(); // получаем главный поток
+//        System.out.println(t.getName()); // main
+
         Thread t = Thread.currentThread(); // получаем главный поток
-        System.out.println(t.getName()); // main
+        System.out.println(t); // main
     }
 }
