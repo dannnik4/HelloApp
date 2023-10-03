ba325caa0f6f69197ca8486857678e2952d044c9
dannnik4 <daniilperstnev2001@gmail.com>
2023-10-03 13:27:00 +0300

    Updated JThread.java

--- src/Chapter9/JThread.java
+++ src/Chapter9/JThread.java
@@ -23,8 +23,13 @@ class Program {
 
     public static void main(String[] args) {
 
+//        System.out.println("Main thread started...");
+//        new JThread("JThread").start();
+//        System.out.println("Main thread finished...");
+
         System.out.println("Main thread started...");
-        new JThread("JThread").start();
+        for(int i=1; i < 6; i++)
+            new JThread("JThread " + i).start();
         System.out.println("Main thread finished...");
     }
 }
