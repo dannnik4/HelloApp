e5271bd4d10a15decc16d331eb78e70f29a5f569
dannnik4 <daniilperstnev2001@gmail.com>
2023-10-04 23:20:23 +0300

    Updated JThread.java

--- src/Chapter9/JThread.java
+++ src/Chapter9/JThread.java
@@ -62,8 +62,23 @@ class Program {
 //            new JThread("JThread " + i).start();
 //        System.out.println("Main thread finished...");
 
+//        System.out.println("Main thread started...");
+//        Thread myThread = new Thread(new MyThread(), "MyThread");
+//        myThread.start();
+//        System.out.println("Main thread finished...");
+
         System.out.println("Main thread started...");
-        Thread myThread = new Thread(new MyThread(), "MyThread");
+        Runnable r = ()->{
+            System.out.printf("%s started... \n", Thread.currentThread().getName());
+            try{
+                Thread.sleep(500);
+            }
+            catch(InterruptedException e){
+                System.out.println("Thread has been interrupted");
+            }
+            System.out.printf("%s finished... \n", Thread.currentThread().getName());
+        };
+        Thread myThread = new Thread(r,"MyThread");
         myThread.start();
         System.out.println("Main thread finished...");
     }
