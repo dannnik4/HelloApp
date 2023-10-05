e16babfefad530211f9629661ca272f5f80f7633
dannnik4 <daniilperstnev2001@gmail.com>
2023-10-05 12:52:50 +0300

    Updated TerminatingThread.java

--- src/Chapter9/TerminatingThread.java
+++ src/Chapter9/TerminatingThread.java
@@ -1,6 +1,26 @@
 package Chapter9;
 
 public class TerminatingThread {
+
+    public void main(String[] args) {
+
+        System.out.println("Main thread started...");
+        MyThread myThread = new MyThread();
+        new Thread(myThread,"MyThread").start();
+
+        try{
+            Thread.sleep(1100);
+
+            myThread.disable();
+
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException e){
+            System.out.println("Thread has been interrupted");
+        }
+        System.out.println("Main thread finished...");
+    }
+
     class MyThread implements Runnable {
 
         private boolean isActive;
