3409345d65b31c7bb677c1a71628a2385c1cf68f
dannnik4 <daniilperstnev2001@gmail.com>
2023-10-07 14:04:38 +0300

    Updated TerminatingThread.java

--- src/Chapter9/TerminatingThread.java
+++ src/Chapter9/TerminatingThread.java
@@ -63,11 +63,27 @@ public class TerminatingThread extends Thread {
 
     public void run() {
 
+//        System.out.printf("%s started... \n", Thread.currentThread().getName());
+//        int counter = 1; // счетчик циклов
+//        while (!isInterrupted()) {
+//
+//            System.out.println("Loop " + counter++);
+//        }
+//        System.out.printf("%s finished... \n", Thread.currentThread().getName());
+
         System.out.printf("%s started... \n", Thread.currentThread().getName());
-        int counter = 1; // счетчик циклов
-        while (!isInterrupted()) {
+        int counter=1; // счетчик циклов
+        while(!isInterrupted()){
 
             System.out.println("Loop " + counter++);
+            try{
+                Thread.sleep(100);
+            }
+            catch(InterruptedException e){
+                System.out.println(getName() + " has been interrupted");
+                System.out.println(isInterrupted());    // false
+                interrupt();    // повторно сбрасываем состояние
+            }
         }
         System.out.printf("%s finished... \n", Thread.currentThread().getName());
     }
