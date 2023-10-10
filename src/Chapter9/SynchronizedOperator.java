fa8b3e749814aba62623d1ca59b2bd27adbf699b
dannnik4 <daniilperstnev2001@gmail.com>
2023-10-10 23:32:39 +0300

    Updated SynchronizedOperator.java

--- src/Chapter9/SynchronizedOperator.java
+++ src/Chapter9/SynchronizedOperator.java
@@ -6,20 +6,31 @@ public class SynchronizedOperator {
 
         public void main(String[] args) {
 
-            CommonResource commonResource= new CommonResource();
-            for (int i = 1; i < 6; i++){
+//            CommonResource commonResource = new CommonResource();
+//            for (int i = 1; i < 6; i++) {
+//
+//                Thread t = new Thread(new CountThread(commonResource));
+//                t.setName("Thread " + i);
+//                t.start();
+//            }
+//        }
+
+            CommonResource commonResource = new CommonResource();
+            for (
+                    int i = 1;
+                    i < 6; i++) {
 
                 Thread t = new Thread(new CountThread(commonResource));
-                t.setName("Thread "+ i);
+                t.setName("Thread " + i);
                 t.start();
             }
         }
     }
 
-    class CommonResource{
-
-        int x=0;
-    }
+//    class CommonResource{
+//
+//        int x=0;
+//    }
 
 //    class CountThread implements Runnable{
 //
@@ -41,25 +52,56 @@ public class SynchronizedOperator {
 //    }
 
 
-    class CountThread implements Runnable{
+//    class CountThread implements Runnable{
+//
+//        CommonResource res;
+//        CountThread(CommonResource res){
+//            this.res=res;
+//        }
+//        public void run(){
+//            synchronized(res){
+//                res.x=1;
+//                for (int i = 1; i < 5; i++){
+//                    System.out.printf("%s %d \n", Thread.currentThread().getName(), res.x);
+//                    res.x++;
+//                    try{
+//                        Thread.sleep(100);
+//                    }
+//                    catch(InterruptedException e){}
+//                }
+//            }
+//        }
+//    }
+//}
 
-        CommonResource res;
-        CountThread(CommonResource res){
-            this.res=res;
-        }
-        public void run(){
-            synchronized(res){
-                res.x=1;
-                for (int i = 1; i < 5; i++){
-                    System.out.printf("%s %d \n", Thread.currentThread().getName(), res.x);
-                    res.x++;
-                    try{
-                        Thread.sleep(100);
-                    }
-                    catch(InterruptedException e){}
+
+    class CommonResource {
+
+        int x;
+
+        synchronized void increment() {
+            x = 1;
+            for (int i = 1; i < 5; i++) {
+                System.out.printf("%s %d \n", Thread.currentThread().getName(), x);
+                x++;
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
                 }
             }
         }
     }
 
+    class CountThread implements Runnable {
+
+        CommonResource res;
+
+        CountThread(CommonResource res) {
+            this.res = res;
+        }
+
+        public void run() {
+            res.increment();
+        }
+    }
 }
