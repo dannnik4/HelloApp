e9b25705472df8fa9c8d4ed77b202dbfb55b4e6e
dannnik4 <daniilperstnev2001@gmail.com>
2023-10-12 23:06:42 +0300

    Added Semaphore.java

---
+++ src/Chapter9/Semaphore.java
@@ -0,0 +1,46 @@
+package Chapter9;
+
+public class Semaphore {
+
+    public static void main(String[] args) {
+
+        java.util.concurrent.Semaphore sem = new java.util.concurrent.Semaphore(1); // 1 разрешение
+        CommonResource res = new CommonResource();
+        new Thread(new CountThread(res, sem, "CountThread 1")).start();
+        new Thread(new CountThread(res, sem, "CountThread 2")).start();
+        new Thread(new CountThread(res, sem, "CountThread 3")).start();
+    }
+}
+class CommonResource{
+
+    int x=0;
+}
+
+class CountThread implements Runnable{
+
+    CommonResource res;
+    java.util.concurrent.Semaphore sem;
+    String name;
+    CountThread(CommonResource res, java.util.concurrent.Semaphore sem, String name){
+        this.res=res;
+        this.sem=sem;
+        this.name=name;
+    }
+
+    public void run(){
+
+        try{
+            System.out.println(name + " ожидает разрешение");
+            sem.acquire();
+            res.x=1;
+            for (int i = 1; i < 5; i++){
+                System.out.println(this.name + ": " + res.x);
+                res.x++;
+                Thread.sleep(100);
+            }
+        }
+        catch(InterruptedException e){System.out.println(e.getMessage());}
+        System.out.println(name + " освобождает разрешение");
+        sem.release();
+    }
+}
