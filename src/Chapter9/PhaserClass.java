7974be246805b040dab3591f1b8c5990a3f243f3
Dannnik <daniilperstnev2001@gmail.com>
2023-10-15 21:55:55 +0300

    Updated PhaserClass.java

--- src/Chapter9/PhaserClass.java
+++ src/Chapter9/PhaserClass.java
@@ -39,14 +39,37 @@ class PhaseThread implements Runnable{
         this.name=n;
         phaser.register();
     }
+//    public void run(){
+//
+//        System.out.println(name + " выполняет фазу " + phaser.getPhase());
+//        phaser.arriveAndAwaitAdvance(); // сообщаем, что первая фаза достигнута
+//
+//        System.out.println(name + " выполняет фазу " + phaser.getPhase());
+//        phaser.arriveAndAwaitAdvance(); // сообщаем, что вторая фаза достигнута
+//
+//        System.out.println(name + " выполняет фазу " + phaser.getPhase());
+//        phaser.arriveAndDeregister(); // сообщаем о завершении фаз и удаляем с регистрации объекты
+//    }
+
     public void run(){
 
         System.out.println(name + " выполняет фазу " + phaser.getPhase());
         phaser.arriveAndAwaitAdvance(); // сообщаем, что первая фаза достигнута
+        try{
+            Thread.sleep(200);
+        }
+        catch(InterruptedException ex){
+            System.out.println(ex.getMessage());
+        }
 
         System.out.println(name + " выполняет фазу " + phaser.getPhase());
         phaser.arriveAndAwaitAdvance(); // сообщаем, что вторая фаза достигнута
-
+        try{
+            Thread.sleep(200);
+        }
+        catch(InterruptedException ex){
+            System.out.println(ex.getMessage());
+        }
         System.out.println(name + " выполняет фазу " + phaser.getPhase());
         phaser.arriveAndDeregister(); // сообщаем о завершении фаз и удаляем с регистрации объекты
     }
