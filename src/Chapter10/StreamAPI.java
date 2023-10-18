48577c7cffb3dde0f6f7b80a872459eea358336d
dannnik4 <daniilperstnev2001@gmail.com>
2023-10-18 23:53:31 +0300

    Added StreamAPI.java

---
+++ src/Chapter10/StreamAPI.java
@@ -0,0 +1,15 @@
+package Chapter10;
+
+public class StreamAPI {
+    public static void main(String[] args) {
+        int[] numbers = {-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5};
+        int count = 0;
+        for (
+                int i : numbers) {
+
+            if (i > 0) count++;
+        }
+        System.out.println(count);
+    }
+
+}
