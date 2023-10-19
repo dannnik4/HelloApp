a2f3100fe9787e277e15075a6340ec1db5a76ce4
dannnik4 <daniilperstnev2001@gmail.com>
2023-10-19 23:39:54 +0300

    Added DataFlow.java

---
+++ src/Chapter10/DataFlow.java
@@ -0,0 +1,16 @@
+package Chapter10;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class DataFlow {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> cities = new ArrayList<String>();
+        Collections.addAll(cities, "Париж", "Лондон", "Мадрид");
+        cities.stream() // получаем поток
+                .filter(s->s.length()==6) // применяем фильтрацию по длине строки
+                .forEach(s->System.out.println(s)); // выводим отфильтрованные строки на консоль
+    }
+}
