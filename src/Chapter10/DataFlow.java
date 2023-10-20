e2c95a934dd2e9344dac2689f4d26800db726a3d
dannnik4 <daniilperstnev2001@gmail.com>
2023-10-20 23:38:23 +0300

    Updated DataFlow.java

--- src/Chapter10/DataFlow.java
+++ src/Chapter10/DataFlow.java
@@ -1,6 +1,7 @@
 package Chapter10;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.stream.Stream;
 
@@ -8,18 +9,21 @@ public class DataFlow {
 
     public static void main(String[] args) {
 
-        ArrayList<String> cities = new ArrayList<String>();
-        Collections.addAll(cities, "Париж", "Лондон", "Мадрид");
-        cities.stream() // получаем поток
-                .filter(s->s.length()==6) // применяем фильтрацию по длине строки
-                .forEach(System.out::println); // выводим отфильтрованные строки на консоль
+//        ArrayList<String> cities = new ArrayList<String>();
+//        Collections.addAll(cities, "Париж", "Лондон", "Мадрид");
+//        cities.stream() // получаем поток
+//                .filter(s->s.length()==6) // применяем фильтрацию по длине строки
+//                .forEach(System.out::println); // выводим отфильтрованные строки на консоль
+//
+//        Stream<String> citiesStream = cities.stream(); // получаем поток
+//        citiesStream = citiesStream.filter(s->s.length()==6); // применяем фильтрацию по длине строки
+//        citiesStream.forEach(System.out::println); // выводим отфильтрованные строки на консоль
+//
+//        long number = citiesStream.count(); // здесь ошибка, так как поток уже употреблен
+//        System.out.println(number);
+//        citiesStream = citiesStream.filter(s->s.length()>5); // тоже нельзя, так как поток уже употреблен
 
-        Stream<String> citiesStream = cities.stream(); // получаем поток
-        citiesStream = citiesStream.filter(s->s.length()==6); // применяем фильтрацию по длине строки
-        citiesStream.forEach(System.out::println); // выводим отфильтрованные строки на консоль
-
-        long number = citiesStream.count(); // здесь ошибка, так как поток уже употреблен
-        System.out.println(number);
-        citiesStream = citiesStream.filter(s->s.length()>5); // тоже нельзя, так как поток уже употреблен
+        Stream<String> citiesStream = Arrays.stream(new String[]{"Париж", "Лондон", "Мадрид"}) ;
+        citiesStream.forEach(System.out::println); // выводим все элементы массива
     }
 }
