f664940ac4c3e4a2eead1a2ff253cdbce184aea3
dannnik4 <daniilperstnev2001@gmail.com>
2023-10-22 22:20:43 +0300

    Updated FilteringDisplay.java

--- src/Chapter10/FilteringDisplay.java
+++ src/Chapter10/FilteringDisplay.java
@@ -10,4 +10,31 @@ public class FilteringDisplay {
         Stream<String> citiesStream = Stream.of("Париж", "Лондон", "Мадрид","Берлин", "Брюссель");
         citiesStream.filter(s->s.length()==6).forEach(System.out::println);
     }
+
+    class Phone{
+
+        private String name;
+        private int price;
+
+        public Phone(String name, int price){
+            this.name=name;
+            this.price=price;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getPrice() {
+            return price;
+        }
+
+        public void setPrice(int price) {
+            this.price = price;
+        }
+    }
 }
