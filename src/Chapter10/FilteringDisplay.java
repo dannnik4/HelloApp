e92cdb64ad824299f3c9c9308c0198897159d0e1
dannnik4 <daniilperstnev2001@gmail.com>
2023-10-22 22:23:09 +0300

    Updated FilteringDisplay.java

--- src/Chapter10/FilteringDisplay.java
+++ src/Chapter10/FilteringDisplay.java
@@ -3,12 +3,17 @@ package Chapter10;
 import java.util.stream.Stream;
 
 public class FilteringDisplay {
-    public static void main(String[] args) {
+    public void main(String[] args) {
 //        Stream<String> citiesStream = Stream.of("Париж", "Лондон", "Мадрид", "Берлин", "Брюссель");
 //        citiesStream.forEach(System.out::println);
 
-        Stream<String> citiesStream = Stream.of("Париж", "Лондон", "Мадрид","Берлин", "Брюссель");
-        citiesStream.filter(s->s.length()==6).forEach(System.out::println);
+//        Stream<String> citiesStream = Stream.of("Париж", "Лондон", "Мадрид","Берлин", "Брюссель");
+//        citiesStream.filter(s->s.length()==6).forEach(System.out::println);
+
+        Stream<Phone> phoneStream = Stream.of(new Phone("iPhone 6 S", 54000), new Phone("Lumia 950", 45000),
+                new Phone("Samsung Galaxy S 6", 40000));
+
+        phoneStream.filter(p->p.getPrice()<50000).forEach(p->System.out.println(p.getName()));
     }
 
     class Phone{
