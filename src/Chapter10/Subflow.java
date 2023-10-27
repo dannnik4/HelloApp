8d5e993bb524192cbec2613541236599e2183d1f
dannnik4 <daniilperstnev2001@gmail.com>
2023-10-27 13:09:12 +0300

    Updated Subflow.java

--- src/Chapter10/Subflow.java
+++ src/Chapter10/Subflow.java
@@ -5,8 +5,12 @@ import java.util.stream.Stream;
 public class Subflow {
     public static void main(String[] args) {
 
+//        Stream<Integer> numbers = Stream.of(-3, -2, -1, 0, 1, 2, 3, -4, -5);
+//        numbers.takeWhile(n -> n < 0)
+//                .forEach(System.out::println);
+
         Stream<Integer> numbers = Stream.of(-3, -2, -1, 0, 1, 2, 3, -4, -5);
-        numbers.takeWhile(n -> n < 0)
+        numbers.sorted().takeWhile(n -> n < 0)
                 .forEach(System.out::println);
     }
 }
