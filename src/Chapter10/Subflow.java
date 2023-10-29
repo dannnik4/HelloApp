d7e3a7f89c0b439b61156901201a8c544ae0e65e
dannnik4 <daniilperstnev2001@gmail.com>
2023-10-29 15:54:54 +0200

    Updated Subflow.java

--- src/Chapter10/Subflow.java
+++ src/Chapter10/Subflow.java
@@ -16,5 +16,10 @@ public class Subflow {
         Stream<Integer> numbers = Stream.of(-3, -2, -1, 0, 1, 2, 3, -4, -5);
         numbers.sorted().dropWhile(n -> n < 0)
                 .forEach(n -> System.out.println(n));
+
+        Stream<String> people1 = Stream.of("Tom", "Bob", "Sam");
+        Stream<String> people2 = Stream.of("Alice", "Kate", "Sam");
+
+        Stream.concat(people1, people2).forEach(n -> System.out.println(n));
     }
 }
