8b9d1d3058cc97852289d39bb0935b79376039d6
dannnik4 <daniilperstnev2001@gmail.com>
2023-11-01 11:54:44 +0200

    Updated MixingOperations.java

--- src/Chapter10/MixingOperations.java
+++ src/Chapter10/MixingOperations.java
@@ -12,12 +12,26 @@ public class MixingOperations {
 //        // количество элементов с длиной не больше 3 символов
 //        System.out.println(names.stream().filter(n->n.length()<=3).count());  // 3
 
+//        ArrayList<String> names = new ArrayList<String>(Arrays.asList(new String[]{"Tom", "Sam", "Bob", "Alice"}));
+//
+//        Optional<String> first = names.stream().findFirst();
+//        System.out.println(first.get());    // Tom
+//
+//        Optional<String> any = names.stream().findAny();
+//        System.out.println(first.get());    // Tom
+
         ArrayList<String> names = new ArrayList<String>(Arrays.asList(new String[]{"Tom", "Sam", "Bob", "Alice"}));
 
-        Optional<String> first = names.stream().findFirst();
-        System.out.println(first.get());    // Tom
+        // есть ли в потоке строка, длина которой больше 3
+        boolean any = names.stream().anyMatch(s->s.length()>3);
+        System.out.println(any);    // true
 
-        Optional<String> any = names.stream().findAny();
-        System.out.println(first.get());    // Tom
+        // все ли строки имеют длину в 3 символа
+        boolean all = names.stream().allMatch(s->s.length()==3);
+        System.out.println(all);    // false
+
+        // НЕТ ЛИ в потоке строки "Bill". Если нет, то true, если есть, то false
+        boolean none = names.stream().noneMatch(s->s=="Bill");
+        System.out.println(none);   // true
     }
 }
