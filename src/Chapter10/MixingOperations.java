f6e7a67e333af3a6c006d87ca03a0248dddbe4f5
dannnik4 <daniilperstnev2001@gmail.com>
2023-11-02 18:39:28 +0200

    Updated MixingOperations.java

--- src/Chapter10/MixingOperations.java
+++ src/Chapter10/MixingOperations.java
@@ -34,12 +34,42 @@ public class MixingOperations {
 //        boolean none = names.stream().noneMatch(s->s=="Bill");
 //        System.out.println(none);   // true
 
-        ArrayList<Integer> numbers = new ArrayList<Integer>();
-        numbers.addAll(Arrays.asList(new Integer[]{1,2,3,4,5,6,7,8,9}));
+//        ArrayList<Integer> numbers = new ArrayList<Integer>();
+//        numbers.addAll(Arrays.asList(new Integer[]{1,2,3,4,5,6,7,8,9}));
+//
+//        Optional<Integer> min = numbers.stream().min(Integer::compare);
+//        Optional<Integer> max = numbers.stream().max(Integer::compare);
+//        System.out.println(min.get());  // 1
+//        System.out.println(max.get());  // 9
 
-        Optional<Integer> min = numbers.stream().min(Integer::compare);
-        Optional<Integer> max = numbers.stream().max(Integer::compare);
-        System.out.println(min.get());  // 1
-        System.out.println(max.get());  // 9
+        ArrayList<Phone1> phones = new ArrayList<Phone1>();
+        phones.addAll(Arrays.asList(new Phone1[]{
+                new Phone1("iPhone 8", 52000),
+                new Phone1("Nokia 9", 35000),
+                new Phone1("Samsung Galaxy S9", 48000),
+                new Phone1("HTC U12", 36000)
+        }));
+
+        Phone1 min = phones.stream().min(Phone1::compare).get();
+        Phone1 max = phones.stream().max(Phone1::compare).get();
+        System.out.printf("MIN Name: %s Price: %d \n", min.getName(), min.getPrice());
+        System.out.printf("MAX Name: %s Price: %d \n", max.getName(), max.getPrice());
     }
+}
+class Phone1{
+
+    private String name;
+    private int price;
+
+    public Phone1(String name, int price){
+        this.name=name;
+        this.price=price;
+    }
+    public static int compare (Phone1 p1, Phone1 p2){
+        if(p1.getPrice() > p2.getPrice())
+            return 1;
+        return -1;
+    }
+    public String getName() { return name; }
+    public int getPrice() { return price;}
 }
