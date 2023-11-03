d38afa1be62ef6031e423f7954e4cc57fb6c24d5
dannnik4 <daniilperstnev2001@gmail.com>
2023-11-03 17:00:59 +0200

    Updated ReduceMethod.java

--- src/Chapter10/ReduceMethod.java
+++ src/Chapter10/ReduceMethod.java
@@ -9,5 +9,9 @@ public class ReduceMethod {
         Stream<Integer> numbersStream = Stream.of(1,2,3,4,5,6);
         Optional<Integer> result = numbersStream.reduce((x, y)->x*y);
         System.out.println(result.get()); // 720
+
+        Stream<String> wordsStream = Stream.of("мама", "мыла", "раму");
+        Optional<String> sentence = wordsStream.reduce((x,y)->x + " " + y);
+        System.out.println(sentence.get());
     }
 }
