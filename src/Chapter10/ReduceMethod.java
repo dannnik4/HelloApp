d9937eca90517c58e327eb9bdeb5fe55a4a11909
dannnik4 <daniilperstnev2001@gmail.com>
2023-11-04 11:00:30 +0200

    Updated ReduceMethod.java

--- src/Chapter10/ReduceMethod.java
+++ src/Chapter10/ReduceMethod.java
@@ -18,6 +18,22 @@ public class ReduceMethod {
         int identity = 1;
         result = Optional.of(numberStream.reduce(identity, (x, y) -> x * y));
         System.out.println(result);  // 24
+
+        Stream<Phone2> phoneStream = Stream.of(new Phone2("iPhone 6 S", 54000),
+                new Phone2("Lumia 950", 45000),
+                new Phone2("Samsung Galaxy S 6", 40000),
+                new Phone2("LG G 4", 32000));
+
+        int sum = phoneStream.reduce(0,
+                (x,y)-> {
+                    if(y.getPrice()<50000)
+                        return x + y.getPrice();
+                    else
+                        return x + 0;
+                },
+                (x, y)->x+y);
+
+        System.out.println(sum); // 117000
     }
 }
 class Phone2{
