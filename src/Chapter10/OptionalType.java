48cef488ac8abac3590c071137b5f25c4a1c719a
dannnik4 <daniilperstnev2001@gmail.com>
2023-11-05 23:35:51 +0200

    Added OptionalType.java

---
+++ src/Chapter10/OptionalType.java
@@ -0,0 +1,15 @@
+package Chapter10;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Optional;
+
+public class OptionalType {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.addAll(Arrays.asList(new Integer[]{1,2,3,4,5,6,7,8,9}));
+        Optional<Integer> min = numbers.stream().min(Integer::compare);
+        System.out.println(min.get());  // 1
+    }
+}
