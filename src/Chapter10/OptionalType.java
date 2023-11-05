57f99707693b26b8eed17467392173e8ce51cf06
dannnik4 <daniilperstnev2001@gmail.com>
2023-11-05 23:36:17 +0200

    Updated OptionalType.java

--- src/Chapter10/OptionalType.java
+++ src/Chapter10/OptionalType.java
@@ -7,9 +7,16 @@ import java.util.Optional;
 public class OptionalType {
     public static void main(String[] args) {
 
+//        ArrayList<Integer> numbers = new ArrayList<Integer>();
+//        numbers.addAll(Arrays.asList(new Integer[]{1,2,3,4,5,6,7,8,9}));
+//        Optional<Integer> min = numbers.stream().min(Integer::compare);
+//        System.out.println(min.get());  // 1
+
         ArrayList<Integer> numbers = new ArrayList<Integer>();
-        numbers.addAll(Arrays.asList(new Integer[]{1,2,3,4,5,6,7,8,9}));
         Optional<Integer> min = numbers.stream().min(Integer::compare);
-        System.out.println(min.get());  // 1
+        if(min.isPresent()){
+
+            System.out.println(min.get());
+        }
     }
 }
