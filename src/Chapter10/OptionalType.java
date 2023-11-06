a08b2f8ce5e5918bf9d19fce8dba3442a1492bfc
dannnik4 <daniilperstnev2001@gmail.com>
2023-11-06 23:31:28 +0200

    Updated OptionalType.java

--- src/Chapter10/OptionalType.java
+++ src/Chapter10/OptionalType.java
@@ -3,6 +3,7 @@ package Chapter10;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.Random;
 
 public class OptionalType {
     public static void main(String[] args) {
@@ -19,14 +20,19 @@ public class OptionalType {
 //            System.out.println(min.get());
 //        }
 
-        // пустой список
+//        // пустой список
+//        ArrayList<Integer> numbers = new ArrayList<Integer>();
+//        Optional<Integer> min = numbers.stream().min(Integer::compare);
+//        System.out.println(min.orElse(-1)); // -1
+//
+//        // непустой список
+//        numbers.addAll(Arrays.asList(new Integer[]{4,5,6,7,8,9}));
+//        min = numbers.stream().min(Integer::compare);
+//        System.out.println(min.orElse(-1)); // 4
+
         ArrayList<Integer> numbers = new ArrayList<Integer>();
         Optional<Integer> min = numbers.stream().min(Integer::compare);
-        System.out.println(min.orElse(-1)); // -1
-
-        // непустой список
-        numbers.addAll(Arrays.asList(new Integer[]{4,5,6,7,8,9}));
-        min = numbers.stream().min(Integer::compare);
-        System.out.println(min.orElse(-1)); // 4
+        Random rnd = new Random();
+        System.out.println(min.orElseGet(()->rnd.nextInt(100)));
     }
 }
