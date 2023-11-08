e10276182915b2b2e266b4dc44f9b78e08b6fd6a
dannnik4 <daniilperstnev2001@gmail.com>
2023-11-08 21:41:17 +0200

    Added CollectMethod.java

---
+++ src/Chapter10/CollectMethod.java
@@ -0,0 +1,17 @@
+package Chapter10;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CollectMethod {
+    public static void main(String[] args) {
+
+        List<String> phones = new ArrayList<String>();
+        Collections.addAll(phones, "iPhone 8", "HTC U12", "Huawei Nexus 6P",
+                "Samsung Galaxy S9", "LG G6", "Xiaomi MI6", "ASUS Zenfone 2",
+                "Sony Xperia Z5", "Meizu Pro 6", "Lenovo S850");
+        }
+    }
+}
