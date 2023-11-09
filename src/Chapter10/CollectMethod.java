0ca62e1d363bc84ad1b49b51333e2fbc22e9accb
dannnik4 <daniilperstnev2001@gmail.com>
2023-11-09 20:47:42 +0200

    Updated CollectMethod.java

--- src/Chapter10/CollectMethod.java
+++ src/Chapter10/CollectMethod.java
@@ -26,4 +26,22 @@ public class CollectMethod {
             System.out.println(s);
         }
     }
+    class Phone3{
+
+        private String name;
+        private int price;
+
+        public Phone3(String name, int price){
+            this.name=name;
+            this.price=price;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getPrice() {
+            return price;
+        }
+    }
 }
