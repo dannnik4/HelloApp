d00357e7a86c0c044172a54190cf687c87a725d4
dannnik4 <daniilperstnev2001@gmail.com>
2023-11-10 12:59:00 +0200

    Updated CollectMethod.java

--- src/Chapter10/CollectMethod.java
+++ src/Chapter10/CollectMethod.java
@@ -1,30 +1,39 @@
 package Chapter10;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CollectMethod {
-    public static void main(String[] args) {
+    public void main(String[] args) {
 
-        List<String> phones = new ArrayList<String>();
-        Collections.addAll(phones, "iPhone 8", "HTC U12", "Huawei Nexus 6P",
-                "Samsung Galaxy S9", "LG G6", "Xiaomi MI6", "ASUS Zenfone 2",
-                "Sony Xperia Z5", "Meizu Pro 6", "Lenovo S850");
+//        List<String> phones = new ArrayList<String>();
+//        Collections.addAll(phones, "iPhone 8", "HTC U12", "Huawei Nexus 6P",
+//                "Samsung Galaxy S9", "LG G6", "Xiaomi MI6", "ASUS Zenfone 2",
+//                "Sony Xperia Z5", "Meizu Pro 6", "Lenovo S850");
 
 //        List<String> filteredPhones = phones.stream()
 //                .filter(s->s.length()<10)
 //                .collect(Collectors.toList());
 
-        Set<String> filteredPhones = phones.stream()
-                .filter(s->s.length()<10)
-                .collect(Collectors.toSet());
+//        Set<String> filteredPhones = phones.stream()
+//                .filter(s->s.length()<10)
+//                .collect(Collectors.toSet());
+//
+//        for(String s : filteredPhones){
+//            System.out.println(s);
+//        }
 
-        for(String s : filteredPhones){
-            System.out.println(s);
-        }
+        Stream<Phone3> phoneStream = Stream.of(new Phone3("iPhone 8", 54000),
+                new Phone3("Nokia 9", 45000),
+                new Phone3("Samsung Galaxy S9", 40000),
+                new Phone3("LG G6", 32000));
+
+
+        Map<String, Integer> phones = phoneStream
+                .collect(Collectors.toMap(p->p.getName(), t->t.getPrice()));
+
+        phones.forEach((k,v)->System.out.println(k + " " + v));
     }
     class Phone3{
 
