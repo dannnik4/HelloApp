408b7c19a4f55d8881437f26ef0e9211ee3d45ba
dannnik4 <daniilperstnev2001@gmail.com>
2023-11-29 23:35:31 +0200

    Added ParallelStreams.java

---
+++ src/Chapter10/ParallelStreams.java
@@ -0,0 +1,13 @@
+package Chapter10;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class ParallelStreams {
+    public static void main(String[] args) {
+
+        Stream<Integer> numbersStream = Stream.of(1, 2, 3, 4, 5, 6);
+        Optional<Integer> result = numbersStream.parallel().reduce((x, y)-> x*y);
+        System.out.println(result.get()); // 720
+    }
+}
