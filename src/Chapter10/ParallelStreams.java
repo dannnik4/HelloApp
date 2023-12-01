abbd4409bcb5a91df14b470e2b4747901ffe9969
dannnik4 <daniilperstnev2001@gmail.com>
2023-12-01 23:03:19 +0200

    Updated ParallelStreams.java

--- src/Chapter10/ParallelStreams.java
+++ src/Chapter10/ParallelStreams.java
@@ -1,13 +1,23 @@
 package Chapter10;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 
 public class ParallelStreams {
     public static void main(String[] args) {
 
-        Stream<Integer> numbersStream = Stream.of(1, 2, 3, 4, 5, 6);
-        Optional<Integer> result = numbersStream.parallel().reduce((x, y)-> x*y);
-        System.out.println(result.get());
+//        Stream<Integer> numbersStream = Stream.of(1, 2, 3, 4, 5, 6);
+//        Optional<Integer> result = numbersStream.parallel().reduce((x, y)-> x*y);
+//        System.out.println(result.get());
+
+        List<String> people = Arrays.asList("Tom","Bob", "Sam", "Kate", "Tim");
+
+        System.out.println("Последовательный поток");
+        people.stream().filter(p->p.length()==3).forEach(System.out::println);
+
+        System.out.println("\nПараллельный поток");
+        people.parallelStream().filter(p->p.length()==3).forEach(System.out::println);
     }
 }
